6e8f0ae266d0f02cf9e0ca18595cd47c6a56796d
Sehoon Han <sha9@gatech.edu>
2011-06-13 16:28:18 -0400

    Urgent:: Add EigenArrayHelper.h

---
+++ src/utils/EigenArrayHelper.h
@@ -0,0 +1,16 @@
+/*
+  RTQL8, Copyright (c) 2011 Georgia Tech Graphics Lab
+  All rights reserved.
+
+  Author	Sehoon Ha
+  Date		06/12/2011
+*/
+
+#ifndef UTILS_EIGEN_ARRAY_HELPER_H
+#define UTILS_EIGEN_ARRAY_HELPER_H
+
+#define EIGEN_VV_VEC3D std::vector< std::vector< Eigen::Vector3d, Eigen::aligned_allocator<Eigen::Vector3d> > >
+
+
+#endif // #ifndef UTILS_EIGEN_ARRAY_HELPER_H
+
