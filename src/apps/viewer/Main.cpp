8251a18a5857e960a5db13a7b5f294824f7c0137
Sehoon Han <sha9@gatech.edu>
2011-06-13 20:50:32 -0400

    Add a viewer application to CMakeLists.txt Fix a initialization bug of mFrame @ class MyWindow

--- src/apps/viewer/Main.cpp
+++ src/apps/viewer/Main.cpp
@@ -1,30 +1,32 @@
 #include "model3d/FileInfoModel.h"
 #include "model3d/FileInfoDof.h"
 #include "MyWindow.h"
-#include <GLUT/glut.h>
+#include "utils/LoadOpengl.h"
+#include <iostream>
+using namespace std;
 
 using namespace model3d;
 
 int main(int argc, char* argv[])
 {
-	if(argc!=3){
-		cout<<"viewer skel_file dof_file"<<endl;
-		exit(0);
-	}
+    if(argc!=3){
+        cout<<"viewer skel_file dof_file"<<endl;
+        exit(0);
+    }
 	
-	FileInfoModel model;
-	model.loadFile(argv[1],FileInfoModel::SKEL);
+    FileInfoModel model;
+    model.loadFile(argv[1],FileInfoModel::SKEL);
 
-	FileInfoDof motion(model.getSkel());
-	motion.loadFile(argv[2]);
+    FileInfoDof motion(model.getSkel());
+    motion.loadFile(argv[2]);
 
 
-	MyWindow window(motion);
-	window.computeMax();
+    MyWindow window(motion);
+    window.computeMax();
 
-	glutInit(&argc, argv);
-	window.initWindow(640, 480, argv[1]);
-	glutMainLoop();
+    glutInit(&argc, argv);
+    window.initWindow(640, 480, argv[1]);
+    glutMainLoop();
 
-	return 0;
+    return 0;
 }
--- src/apps/viewer/MyWindow.cpp
+++ src/apps/viewer/MyWindow.cpp
@@ -1,7 +1,9 @@
 #include "MyWindow.h"
 #include "YUI/glFuncs.h"
-#include <GLUT/glut.h>
-#include "model3d/skeleton.h"
+// #include <GLUT/glut.h>
+#include "utils/LoadOpengl.h"
+#include "model3d/Skeleton.h"
+#include <cstdio>
 
 using namespace std;
 using namespace Eigen;
@@ -9,84 +11,84 @@ using namespace model3d;
 
 void MyWindow::displayTimer(int val)
 {
-	mFrame++;
-	if(mFrame == mMaxFrame){
-		mFrame = 0;
-		mPlaying = false;
-	}
-	glutPostRedisplay();
-	if(mPlaying)	
-		glutTimerFunc(mDisplayTimeout, refreshTimer, val);
+    mFrame++;
+    if(mFrame == mMaxFrame){
+        mFrame = 0;
+        mPlaying = false;
+    }
+    glutPostRedisplay();
+    if(mPlaying)	
+        glutTimerFunc(mDisplayTimeout, refreshTimer, val);
 }
 
 void MyWindow::computeMax()
 {
-	mMaxFrame = mMotion.getNumFrames();
+    mMaxFrame = mMotion.getNumFrames();
 }
 
 void MyWindow::draw()
 {
-	Skeleton* model = mMotion.getModel();
-	int nFrames = mMotion.getNumFrames();
-	int fr = mFrame;
-	if(fr>=nFrames) fr = nFrames-1;
-	model->setPose(mMotion.mDofs.at(fr));
-	model->draw(&mRenderer, Vector4d(0.0,0.0,0.0,1.0),false);
-	if(bMarker)
-		model->drawHandles(&mRenderer, Vector4d(1.0,0.0,0.0,1.0),false);
+    Skeleton* model = mMotion.getModel();
+    int nFrames = mMotion.getNumFrames();
+    int fr = mFrame;
+    if(fr>=nFrames) fr = nFrames-1;
+    model->setPose(mMotion.mDofs.at(fr));
+    model->draw(&mRenderer, Vector4d(0.0,0.0,0.0,1.0),false);
+    if(bMarker)
+        model->drawHandles(&mRenderer, Vector4d(1.0,0.0,0.0,1.0),false);
 
-	if(bShowProgress) drawProgressBar(fr,mMaxFrame);
+    if(bShowProgress) drawProgressBar(fr,mMaxFrame);
 
-	char buff[64];
-	sprintf(buff,"%d/%d",mFrame,mMaxFrame);
-	string frame(buff);
-	glDisable(GL_LIGHTING);
-	glColor3f(0.0,0.0,0.0);
-	drawStringOnScreen(0.02f,0.02f,frame);
-	glEnable(GL_LIGHTING);
+    char buff[64];
+    sprintf(buff,"%d/%d",mFrame,mMaxFrame);
+    string frame(buff);
+    glDisable(GL_LIGHTING);
+    glColor3f(0.0,0.0,0.0);
+    drawStringOnScreen(0.02f,0.02f,frame);
+    glEnable(GL_LIGHTING);
 }
 
 void MyWindow::keyboard(unsigned char key, int x, int y)
 {
-	switch(key){
-	case ' ':
-		mPlaying = !mPlaying;
-		if(mPlaying)
-			glutTimerFunc( mDisplayTimeout, refreshTimer, 0);
-		break;
-	case 'r':
-		mFrame = 0;
-		break;
-	case 'a':
-		if(mFrame<mMaxFrame-1)
-			mFrame ++;
-		break;
-	case 'd':
-		if(mFrame>0)
-			mFrame --;
-		break;
-	case 'm':
-		bMarker = !bMarker;
-		break;
-	default:
-		Win3D::keyboard(key,x,y);
-	}
-	glutPostRedisplay();
+    switch(key){
+    case ' ':
+        mPlaying = !mPlaying;
+        if(mPlaying)
+            glutTimerFunc( mDisplayTimeout, refreshTimer, 0);
+        break;
+    case 'r':
+        mFrame = 0;
+        break;
+    case 'a':
+        if(mFrame<mMaxFrame-1)
+            mFrame ++;
+        break;
+    case 'd':
+        if(mFrame>0)
+            mFrame --;
+        break;
+    case 'm':
+        bMarker = !bMarker;
+        break;
+    default:
+        Win3D::keyboard(key,x,y);
+    }
+    glutPostRedisplay();
 }
 
 void MyWindow::move(int x, int y)
 {
-	double Xport = (double)x/mWinWidth;
-	double Yport = (double)(mWinHeight-y)/mWinHeight;
+    double Xport = (double)x/mWinWidth;
+    double Yport = (double)(mWinHeight-y)/mWinHeight;
 
-	bool oldShow = bShowProgress;
+    bool oldShow = bShowProgress;
 
-	if(!mRotate && !mTranslate && !mZooming){
-		if(Xport>=0.15 && Xport<=0.85 && Yport>=0.02 && Yport<=0.08)
-			bShowProgress = true;
-		else if(bShowProgress) bShowProgress = false;
-	}
+    if(!mRotate && !mTranslate && !mZooming){
+        if(Xport>=0.15 && Xport<=0.85 && Yport>=0.02 && Yport<=0.08)
+            bShowProgress = true;
+        else if(bShowProgress) bShowProgress = false;
+    }
 
-	if( oldShow != bShowProgress)
-		glutPostRedisplay();
+    if( oldShow != bShowProgress)
+        glutPostRedisplay();
 }
