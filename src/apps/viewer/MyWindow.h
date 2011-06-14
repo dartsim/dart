0d99952eccd85a3380b6ed09bebd230bc5942c3e
Sehoon Han <sha9@gatech.edu>
2011-06-13 20:50:32 -0400

    Add a viewer application to CMakeLists.txt Fix a initialization bug of mFrame @ class MyWindow

--- src/apps/viewer/MyWindow.h
+++ src/apps/viewer/MyWindow.h
@@ -5,39 +5,38 @@
 #include "model3d/FileInfoDof.h"
 #include "renderer/OpenGLRenderInterface.h"
 
-class MyWindow : public Win3D
-{
+class MyWindow : public Win3D {
 public:
-	MyWindow(model3d::FileInfoDof& mot): Win3D(), mMotion(mot)
-	{
-		mBackground[0] = 1.0;
-		mBackground[1] = 1.0;
-		mBackground[2] = 1.0;
-		mBackground[3] = 1.0;
+MyWindow(model3d::FileInfoDof& mot): Win3D(), mMotion(mot) {
+        mBackground[0] = 1.0;
+        mBackground[1] = 1.0;
+        mBackground[2] = 1.0;
+        mBackground[3] = 1.0;
 		
-		mPlaying = false;
-		bMarker = false;
-		bShowProgress = false;
+        mPlaying = false;
+        bMarker = false;
+        bShowProgress = false;
 
-		mPersp = 30.f;
-		mTrans[2] = -1.f;
-	}
+        mPersp = 30.f;
+        mTrans[2] = -1.f;
+        mFrame = 0;
+    }
 
-	virtual void draw();
-	virtual void keyboard(unsigned char key, int x, int y);
-	virtual void displayTimer(int val);
-	virtual void move(int x, int y);
-	void computeMax();
+    virtual void draw();
+    virtual void keyboard(unsigned char key, int x, int y);
+    virtual void displayTimer(int val);
+    virtual void move(int x, int y);
+    void computeMax();
 	
 protected:	
 
-	int mMaxFrame;
-	bool mPlaying;
-	int mFrame;
-	bool bMarker;
-	bool bShowProgress;
-	model3d::FileInfoDof& mMotion;
-	Renderer::OpenGLRenderInterface mRenderer;
+    int mMaxFrame;
+    bool mPlaying;
+    int mFrame;
+    bool bMarker;
+    bool bShowProgress;
+    model3d::FileInfoDof& mMotion;
+    Renderer::OpenGLRenderInterface mRenderer;
 };
 
 #endif
