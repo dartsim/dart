6ed43942f816fcf111ccecb44b3f4ff1921fdeef
Yuting Ye <yuting@cc.gatech.edu>
2011-06-16 18:50:45 -0400

    modified the apps according to model3d

--- src/apps/loadModel/Main.cpp
+++ src/apps/loadModel/Main.cpp
@@ -3,15 +3,12 @@
 
 using namespace model3d;
 
-Vector3d gravity(0,0,0);
-
 int main(int argc, char* argv[]) {
     FileInfoModel modelFile;
     modelFile.loadFile("Yuting.vsk", FileInfoModel::VSK);
     // modelFile.loadFile("Yuting.skel",FileInfoModel::SKEL);
-    
 
-    MyWindow window(modelFile.getSkel());
+    MyWindow window(modelFile.getModel());
 
     glutInit(&argc, argv);
     window.initWindow(640,480,"Load Model");
