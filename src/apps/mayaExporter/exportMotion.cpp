802af19a73d7bdd33ebe24a4e77751d593536218
Sehoon Han <sha9@gatech.edu>
2011-09-12 13:07:52 -0400

    move mayaexports namespace under utils namespace. Now you can access the mayaexports as utils::mayaexports::....

--- src/apps/mayaExporter/exportMotion.cpp
+++ src/apps/mayaExporter/exportMotion.cpp
@@ -1,4 +1,4 @@
-#include "mayaexports/MayaExportMotion.h"
+#include "utils/MayaExportMotion.h"
 #include "kinematics/FileInfoSkel.hpp"
 #include "kinematics/FileInfoDof.h"
 #include "utils/Paths.h"
@@ -13,7 +13,7 @@ int main(int argc, char* argv[])
     FileInfoDof motion(model.getSkel());
     motion.loadFile(GROUNDZERO_DATA_PATH"dof/RHand.dof");
 
-    mayaexports::MayaExportMotion exporter(model.getSkel(),&motion);
+    utils::mayaexports::MayaExportMotion exporter(model.getSkel(),&motion);
     exporter.exportMayaAnim("motion.anim", 0, motion.getNumFrames()-1, "", model.getSkel()->getNumNodes());
 
     return 0;
--- src/apps/mayaExporter/exportSkeleton.cpp
+++ src/apps/mayaExporter/exportSkeleton.cpp
@@ -1,4 +1,4 @@
-#include "mayaexports/MayaExportSkeleton.h"
+#include "utils/MayaExportSkeleton.h"
 #include "kinematics/FileInfoSkel.hpp"
 #include "utils/Paths.h"
 
@@ -9,7 +9,7 @@ int main(int argc, char* argv[])
     FileInfoSkel<Skeleton> model;
     model.loadFile(GROUNDZERO_DATA_PATH"skel/YutingEuler.skel", kinematics::SKEL);
     ofstream out("model.ma");
-    mayaexports::MayaExportSkeleton::exportMayaAscii(model.getSkel(), out);
+    utils::mayaexports::MayaExportSkeleton::exportMayaAscii(model.getSkel(), out);
     out.close();
 
     return 0;
