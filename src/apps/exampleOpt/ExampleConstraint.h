88240e39c9a9f0f059843083649c49ec7951fb42
Sehoon Han <sha9@gatech.edu>
2011-09-12 13:50:13 -0400

    Clear and add a files

---
+++ src/apps/exampleOpt/ExampleConstraint.h
@@ -0,0 +1,24 @@
+#ifndef EXAMPLE_CONSTRAINT_H
+#define EXAMPLE_CONSTRAINT_H
+
+#include "optimizer/Constraint.h"
+namespace optimizer {
+    class Var;
+} // namespace optimizer
+
+class ExampleConstraint : public optimizer::Constraint {
+public:
+    ExampleConstraint(std::vector<optimizer::Var *>& var, int index, double target);
+    
+    virtual Eigen::VectorXd evalCon();
+
+    virtual void fillJac(optimizer::VVD, int index) {}
+    virtual void fillJac(optimizer::VVD, optimizer::VVB, int index) {}
+    virtual void fillObjGrad(std::vector<double>& dG);
+
+private:
+    int mIndex;
+    double mTarget;
+};
+
+#endif
