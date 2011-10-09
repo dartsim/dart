cf171112e599dae9933448c451c865e2f6a067f9
Ana <ahuaman3@gatech.edu>
2011-10-09 17:52:33 -0400

    Added planning library

---
+++ src/planning/Object.cpp
@@ -0,0 +1,36 @@
+/**
+ * @file Object.cpp
+ * @brief Implementation of Object subclass (from Skeleton )
+ */
+#include "Object.h"
+
+using namespace std;
+using namespace Eigen;
+
+namespace planning {
+
+    /**
+     * @function Object
+     * @brief Constructor
+     */
+    Object::Object( ) {
+    }
+
+    /**
+     * @function ~Object
+     * @brief Destructor
+     */
+    Object::~Object() {
+    
+    }
+
+    /**
+     * @function loadModel
+     * @brief Load a 3DS model as a primitive
+     */
+    void Object::loadModel( std::string _filename )
+    {
+    }
+ 
+
+}  // namespace planning
---
+++ src/planning/World.cpp
@@ -0,0 +1,67 @@
+/**
+ * @file World.cpp
+ */
+
+#include <iostream>
+#include "World.h"
+#include "kinematics/Skeleton.h"
+#include "kinematics/BodyNode.h"
+
+using namespace std;
+using namespace Eigen;
+
+namespace planning {
+
+    /**
+     * @function World
+     * @brief Constructor
+     */
+    World::World() {
+        mRobots.resize(0); mObjects.resize(0);
+    }
+
+    /**
+     * @function ~World
+     * @brief Destructor
+     */
+    World::~World() {
+        for( unsigned int i = 0; i < mRobots.size(); i++ ) delete mRobots[i];
+        mRobots.clear();
+        for( unsigned int i = 0; i < mObjects.size(); i++ ) delete mObjects[i];
+        mObjects.clear();      
+    }
+
+    /**
+     * @function addRobot
+     */
+    int World::addRobot( Robot *_robot ) {
+        mRobots.push_back( _robot );
+        return mRobots.size();
+    }
+
+    /**
+     * @function addObject
+     */
+    int World::addObject( Object *_object ) {
+        mObjects.push_back( _object );
+        return mObjects.size();
+    } 
+
+    /**
+     * @function printInfo
+     */
+    void World::printInfo() {
+
+        /// Robots
+        std::cout<<"-- World has " << mRobots.size() << "robots "<< std::endl;
+        for( unsigned int i = 0; i < mRobots.size(); i++ )
+        { std::cout<<"* Robot["<<i<<"]: " << mRobots[i]->mName << std::endl; }
+
+        /// Objects
+        std::cout<<"-- World has " << mObjects.size() << "objects "<< std::endl;
+        for( unsigned int i = 0; i < mObjects.size(); i++ )
+        { std::cout<<"* Object["<<i<<"]: " << mObjects[i]->mName << std::endl; }
+
+    }   
+
+}  // namespace planning
