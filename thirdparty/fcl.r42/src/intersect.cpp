7eb49db3af0d57bd6f0fc8c52a41797f695f2e2a
Mike <mstilman@cc.gatech.edu>
2011-10-24 10:49:20 -0400

    Fixed isnan / _isnan issue in fcl for Win compatibility

--- thirdparty/fcl.r42/src/intersect.cpp
+++ thirdparty/fcl.r42/src/intersect.cpp
@@ -39,6 +39,10 @@
 #include <limits>
 #include <vector>
 
+#ifndef isnan
+#define isnan _isnan
+#endif
+
 namespace fcl
 {
 const BVH_REAL PolySolver::NEAR_ZERO_THRESHOLD = 1e-9;
@@ -1850,7 +1854,7 @@ void TriangleDistance::segPoints(const Vec3f& P, const Vec3f& A, const Vec3f& Q,
 
   // clamp result so t is on the segment P,A
 
-  if((t < 0) || _isnan(t)) t = 0; else if(t > 1) t = 1;
+  if((t < 0) || isnan(t)) t = 0; else if(t > 1) t = 1;
 
   // find u for point on ray Q,B closest to point at t
 
@@ -1860,13 +1864,13 @@ void TriangleDistance::segPoints(const Vec3f& P, const Vec3f& A, const Vec3f& Q,
   // closest points, otherwise, clamp u, recompute and
   // clamp t
 
-  if((u <= 0) || _isnan(u))
+  if((u <= 0) || isnan(u))
   {
     Y = Q;
 
     t = A_dot_T / A_dot_A;
 
-    if((t <= 0) || _isnan(t))
+    if((t <= 0) || isnan(t))
     {
       X = P;
       VEC = Q - P;
@@ -1889,7 +1893,7 @@ void TriangleDistance::segPoints(const Vec3f& P, const Vec3f& A, const Vec3f& Q,
 
     t = (A_dot_B + A_dot_T) / A_dot_A;
 
-    if((t <= 0) || _isnan(t))
+    if((t <= 0) || isnan(t))
     {
       X = P;
       VEC = Y - P;
@@ -1911,7 +1915,7 @@ void TriangleDistance::segPoints(const Vec3f& P, const Vec3f& A, const Vec3f& Q,
   {
     Y = Q + B * u;
 
-    if((t <= 0) || _isnan(t))
+    if((t <= 0) || isnan(t))
     {
       X = P;
       TMP = T.cross(B);
