37e568156917bb6200fc33dbf07df5757d519905
kasiu <kasiu@gatech.edu>
2011-11-08 21:35:07 -0500

    Added LCP problem/matrix construction. Needs test.

--- src/dynamics/ContactDynamics.h
+++ src/dynamics/ContactDynamics.h
@@ -35,23 +35,44 @@ namespace dynamics {
         void destroy();
         
         void fillMatrices();
-        void solve();
+        bool solve();
         void applyExternalForces();
 
         inline int getNumSkels() const { return mSkels.size(); }
         inline int getNumTotalDofs() const { return mIndices[getNumSkels()]; }
+        inline int getNumContactDirections() const { return mNumDir; }
         int getNumContacts() const;
 
         Eigen::MatrixXd getJacobian(kinematics::BodyNode* node,
                                     const Eigen::Vector3d& p) const;
-        Eigen::MatrixXd getNormalMatirx() const;
+
+        // Helper functions to compute all of the matrices
+        // Notation is similar to that used in derivation:
+        // Mqddot + Cqdot + kq = tau + (J^T)(f_n)N + (J^T)D(f_d) -> Mqdot = tau* + N(f_n) + B(f_d)
+        inline Eigen::MatrixXd getMassMatrix() const { return mM; } // M matrix
+        inline Eigen::VectorXd getTauStarVector() const { return mTauStar; } // T* vector (not T)
+        Eigen::MatrixXd getNormalMatrix() const; // N matrix
+        Eigen::MatrixXd getBasisMatrix() const; // B matrix 
+        Eigen::MatrixXd getTangentBasisMatrix(const Eigen::Vector3d& p, const Eigen::Vector3d& n) const; // gets a matrix of tangent dirs.
+        Eigen::MatrixXd getContactMatrix() const; // E matrix
+        Eigen::MatrixXd getMuMatrix() const; // mu matrix
         
         std::vector<SkeletonDynamics*> mSkels;
         std::vector<int> mBodyIndexToSkelIndex;
         std::vector<int> mIndices;
         collision_checking::SkeletonCollision* mCollision;
-        double mDt;
-        double mMu;
+        double mDt; // timestep
+        double mMu; // friction coeff.
+        int mNumDir; // number of basis directions
+
+        // Cached (aggregated) mass/tau matrices
+        Eigen::MatrixXd mM;
+        Eigen::VectorXd mTauStar;
+
+        // Matrices to pass to solver
+        Eigen::MatrixXd mA;
+        Eigen::VectorXd mQBar;
+        Eigen::VectorXd mX;
     }; 
 } // namespace dynamics
 
