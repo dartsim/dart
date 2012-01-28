b1d0bc4d08d9599fd61cd8cf8e207cea362b74e0
Karen Liu <ckarenliu@gmail.com>
2012-01-28 07:23:36 -0500

    added cubes

---
+++ src/apps/cubes/Main.cpp
@@ -0,0 +1,37 @@
+#include "MyWindow.h"
+#include "dynamics/BodyNodeDynamics.h"
+#include "dynamics/SkeletonDynamics.h"
+#include "kinematics/FileInfoSkel.hpp"
+#include "utils/Paths.h"
+
+
+
+using namespace std;
+using namespace Eigen;
+using namespace kinematics;
+using namespace dynamics;
+
+int main(int argc, char* argv[])
+{
+    FileInfoSkel<SkeletonDynamics> model, model2, model3, model4;
+    model.loadFile(DART_DATA_PATH"/skel/ground1.skel", kinematics::SKEL);
+    model2.loadFile(DART_DATA_PATH"/skel/cube1.skel", kinematics::SKEL);
+    model3.loadFile(DART_DATA_PATH"/skel/cube1.skel", kinematics::SKEL);
+    model4.loadFile(DART_DATA_PATH"/skel/cube1.skel", kinematics::SKEL);
+
+    MyWindow window((SkeletonDynamics*)model.getSkel(), (SkeletonDynamics*)model2.getSkel(), (SkeletonDynamics*)model3.getSkel(), (SkeletonDynamics*)model4.getSkel(), NULL);
+
+    cout << "space bar: simulation on/off" << endl;
+    cout << "'s': simulate one step" << endl;
+    cout << "'p': playback/stop" << endl;
+    cout << "'[' and ']': play one frame backward and forward" << endl;
+    cout << "'v': visualization on/off" << endl;
+    cout << "'1' and '2': programmed interaction" << endl;
+    
+   
+    glutInit(&argc, argv);
+    window.initWindow(640, 480, "Cubes");
+    glutMainLoop();
+
+    return 0;
+}
