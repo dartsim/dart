5c4bdf283ffd23c9dd6ea63e0a44eac64ef9672d
Nick Levesque <nlevesque3@gmail.com>
2012-02-05 14:05:40 -0500

    Ground model added

--- src/apps/superCoolSimulation/Main.cpp
+++ src/apps/superCoolSimulation/Main.cpp
@@ -12,10 +12,11 @@ using namespace dynamics;
 
 int main(int argc, char* argv[])
 {
-	FileInfoSkel<SkeletonDynamics> model;
+	FileInfoSkel<SkeletonDynamics> model, model2;
     model.loadFile(DART_DATA_PATH"/skel/jellyfishMan.skel", kinematics::SKEL);
+	model2.loadFile(DART_DATA_PATH"/skel/ground1.skel", kinematics::SKEL);
 
-    MyWindow window((SkeletonDynamics*)model.getSkel());
+    MyWindow window((SkeletonDynamics*)model.getSkel(), (SkeletonDynamics*)model2.getSkel());
     
     glutInit(&argc, argv);
     window.initWindow(640,480,"Super Cool Simulation");
--- src/apps/superCoolSimulation/MyWindow.cpp
+++ src/apps/superCoolSimulation/MyWindow.cpp
@@ -1,29 +1,39 @@
 #include "MyWindow.h"
-#include "dynamics/SkeletonDynamics.h"
+#include "dynamics/BodyNodeDynamics.h"
+#include "dynamics/ContactDynamics.h"
+#include "kinematics/Dof.h"
 #include "utils/UtilsMath.h"
 #include "utils/Timer.h"
 #include "yui/GLFuncs.h"
 #include <cstdio>
+#include "kinematics/BodyNode.h"
 
 using namespace std;
 using namespace Eigen;
 using namespace kinematics;
 using namespace utils;
 using namespace integration;
+using namespace dynamics;
 
 //Constructor
-MyWindow::MyWindow(dynamics::SkeletonDynamics* _m)
-	: Win3D(), mModel(_m)
+MyWindow::MyWindow(dynamics::SkeletonDynamics* _m,dynamics::SkeletonDynamics* _m2)
+	: Win3D(), mModel(_m), mModel2(_m2)
 {
 	//Background Color
 	mBackground[0] = 1.0;
 	mBackground[1] = 1.0;
 	mBackground[2] = 1.0;
 	mBackground[3] = 1.0;
-
+	mFrame = 0;
 	//Camera settings
 	mPersp = 45.0f;
-	mTrans[1] = 300.0f;
+	mTrans[2] = -20.0f;
+	mSim = false;
+	mPlay = false;
+
+	//setup skels vector
+	mSkels.push_back(_m);
+	mSkels.push_back(_m2);
 
 	//Environment Settings
 	mGravity = Vector3d(0.0,-9.8,0.0);
@@ -34,40 +44,91 @@ MyWindow::MyWindow(dynamics::SkeletonDynamics* _m)
 void MyWindow::initDyn()
 {
 	// initialize degrees of freedom
-	mDofs.resize(mModel->getNumDofs());
-	mDofVels.resize(mDofs.size());
-	for(int i=0; i<mModel->getNumDofs(); i++)
-	{
-		mDofs[i] = i/10.0 - 0.5;
-		mDofVels[i] = 0.1;
-	}
-	mModel->setPose(mDofs,false,false);
+	mDofs.resize(2);
+	mDofVels.resize(2);
+
+	// Initialize JellyMan
+	mDofs[0].resize(mModel->getNumDofs());
+	mDofVels[0].resize(mDofs[0].size());
+	mDofs[0].setZero();
+	mDofVels[0].setZero();
+
+	// translate JellyMan
+	mDofs[0][1] = 0.5;
+	mDofs[0][15] = 0.3;
+	mDofs[0][16] = -0.2;
+	mDofs[0][17] = 0.15;
+	mDofs[0][18] = 0.4;
+
+	// Initialize Ground
+	mDofs[1].resize(mModel2->getNumDofs());
+	mDofVels[1].resize(mDofs[0].size());
+	mDofs[1].setZero();
+	mDofVels[1].setZero();
+
+	// translate Ground
+	mDofs[1][1] = -0.5;
+
+	mModel->setPose(mDofs[0],false,false);
+	mModel2->setPose(mDofs[1],false,false);
+
+	mCollisionHandle = new dynamics::ContactDynamics(mSkels,mTimeStep);
+	
 }
 
 VectorXd MyWindow::getState() {
-    VectorXd state(mDofs.size() + mDofVels.size());
-    state.head(mDofs.size()) = mDofs;
-    state.tail(mDofVels.size()) = mDofVels;
+	int stateSize = mDofs[0].size() + mDofs[1].size() + 
+		mDofVels[0].size() + mDofVels[1].size();
+    VectorXd state(stateSize);
+
+	int offset = 0;
+	state.head(mDofs[0].size()) = mDofs[0];
+	
+	offset += mDofs[0].size();
+	state.segment(offset,mDofVels[0].size()) = mDofVels[0];
+
+	offset += mDofVels[0].size();
+	state.segment(offset,mDofs[1].size()) = mDofs[1];
+
+	offset += mDofs[1].size();
+	state.segment(offset,mDofVels[1].size()) = mDofVels[1];
+
     return state;
 }
 
 VectorXd MyWindow::evalDeriv() {
-    VectorXd deriv(mDofs.size() + mDofVels.size());
-    VectorXd qddot = -mModel->getMassMatrix().fullPivHouseholderQr().solve( mModel->getCombinedVector() ); 
-    mModel->clampRotation(mDofs, mDofVels);
-    deriv.tail(mDofVels.size()) = qddot; // set qddot (accelerations)
-    deriv.head(mDofs.size()) = (mDofVels + (qddot * mTimeStep)); // set new velocities
+	int stateSize = mDofs[0].size() + mDofs[1].size() + 
+		mDofVels[0].size() + mDofVels[1].size();
+
+    VectorXd deriv = VectorXd::Zero(stateSize);
+
+    VectorXd qddot = -mModel->getMassMatrix().fullPivHouseholderQr().solve( mModel->getCombinedVector() + mCollisionHandle->getConstraintForce(0) ); 
+    mModel->clampRotation(mDofs[0], mDofVels[0]);
+	deriv.segment(mDofs[0].size(), mDofVels[0].size()) = qddot; // set qddot (accelerations)
+    deriv.head(mDofs[0].size()) = (mDofVels[0] + (qddot * mTimeStep)); // set new velocities
     return deriv;
 }
 
 void MyWindow::setState(VectorXd newState) {
-    mDofVels = newState.tail(mDofVels.size());
-    mDofs = newState.head(mDofs.size());
+	int offset = 0;
+	mDofs[0] = newState.head(mDofs[0].size());
+	
+	offset += mDofs[0].size();
+	mDofVels[0] = newState.segment(offset,mDofVels[0].size());
+
+	offset += mDofVels[0].size();
+	mDofs[1] = newState.segment(offset,mDofs[1].size());
+
+	offset += mDofs[1].size();
+	mDofVels[1] = newState.segment(offset,mDofVels[1].size());
+
 }
 
 void MyWindow::setPose() {
-    mModel->setPose(mDofs,false,false);
-    mModel->computeDynamics(mGravity, mDofVels, true);
+    mModel->setPose(mDofs[0],true,true);
+    mModel->computeDynamics(mGravity, mDofVels[0], true);
+
+	mModel2->setPose(mDofs[1],true,false);
 }
 
 void MyWindow::displayTimer(int _val)
@@ -83,13 +144,13 @@ void MyWindow::displayTimer(int _val)
 
     mFrame += numIter;  
 	glutPostRedisplay();
-	glutTimerFunc(mDisplayTimeout, refreshTimer, 0);
+	glutTimerFunc(mDisplayTimeout, refreshTimer, _val);
 }
 
 void MyWindow::draw()
 {
 	mModel->draw(mRI);
-
+	mModel2->draw(mRI);
 	//display frame count
 	char buff[64];
     sprintf(buff,"%d",mFrame);
@@ -105,6 +166,20 @@ void MyWindow::keyboard(unsigned char key, int x, int y)
 	switch(key){
     case 'r':
         mFrame = 0;
+        break;
+    case ' ': // use space key to play or stop the motion
+        mSim = !mSim;
+        if (mSim) {
+			mPlay = false;
+            glutTimerFunc( mDisplayTimeout, refreshTimer, 0);
+        }
+        break;	
+		case 'p': // playBack
+        mPlay = !mPlay;
+        if (mPlay) {
+            mSim = false;
+            glutTimerFunc( mDisplayTimeout, refreshTimer, 0);
+        }
         break;
     default:
         Win3D::keyboard(key,x,y);
