ff4f0b08ae120e3523aec7ac199cf34906c47e58
Karen Liu <ckarenliu@gmail.com>
2012-03-31 07:11:01 -0400

    detected bugs in cubes

--- src/apps/cubes/MyWindow.cpp
+++ src/apps/cubes/MyWindow.cpp
@@ -27,7 +27,7 @@ void MyWindow::initDyn()
     }
 
     mDofs[0][1] = -0.35;
-    mDofs[1][0] = 0.1;
+    mDofs[1][0] = 0.06;
     mDofs[1][1] = -0.2;
     mDofs[2][0] = 0.05;
     mDofs[3][1] = 0.08;
