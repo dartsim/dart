35700a82927150b7f55c28d12ecde2841be22dba
Ana Huaman <ahuaman3@gatech.edu>
2012-08-11 15:35:12 -0400

    Added red ball example in DART

--- src/apps/HandSPD/Main.cpp
+++ src/apps/HandSPD/Main.cpp
@@ -1,3 +1,11 @@
+/**
+ * @file Main.cpp
+ * @author Karen Liu
+ * @author Can Erdogan - just comments
+ * @date Aug 11, 2012
+ * @brief The executable for the hand example that demonstrates the use of spd controller.
+ */
+
 #include "MyWindow.h"
 #include "kinematics/FileInfoSkel.hpp"
 #include "utils/Paths.h"
@@ -5,28 +13,34 @@
 using namespace kinematics;
 using namespace dynamics;
 
-int main(int argc, char* argv[])
-{
-    const char* modelfile;
-    const char* doffile;
-    if(argc!=3){
-        modelfile = DART_DATA_PATH"skel/fixedHand.skel";
-        doffile = DART_DATA_PATH"dof/fixedHand.dof";
-    }else{
-        modelfile = argv[1];
-        doffile = argv[2];
-    }
+int main(int argc, char* argv[]) {
 
-    FileInfoSkel<SkeletonDynamics> model;
-    model.loadFile(modelfile, SKEL);
-    FileInfoDof *motion = new FileInfoDof(model.getSkel());
-    motion->loadFile(doffile);
+	// Find the model and DOF files
+	const char* modelfile;
+	const char* doffile;
+	if(argc != 3) {
+		modelfile = DART_DATA_PATH"skel/fixedHand.skel";
+		doffile = DART_DATA_PATH"dof/fixedHand.dof";
+	} else {
+		modelfile = argv[1];
+		doffile = argv[2];
+	}
 
-    MyWindow window(motion, (SkeletonDynamics*)model.getSkel(), NULL);    
-   
-    glutInit(&argc, argv);
-    window.initWindow(640, 480, "PD Control");
-    glutMainLoop();
+	// Load the model
+	FileInfoSkel <SkeletonDynamics> model;
+	model.loadFile(modelfile, SKEL);
 
-    return 0;
+	// Load the motion
+	FileInfoDof *motion = new FileInfoDof(model.getSkel());
+	motion->loadFile(doffile);
+
+	// Create the user interface
+	MyWindow window(motion, (SkeletonDynamics*) model.getSkel(), NULL);
+
+	// Start with OpenGL
+	glutInit(&argc, argv);
+	window.initWindow(640, 480, "PD Control");
+	glutMainLoop();
+
+	return 0;
 }
--- src/apps/HandSPD/MyWindow.h
+++ src/apps/HandSPD/MyWindow.h
@@ -2,6 +2,7 @@
 #define _MYWINDOW_
 
 #include <stdarg.h>
+#include <stdio.h>
 #include "yui/Win3D.h"
 #include "integration/EulerIntegrator.h"
 #include "integration/RK4Integrator.h"
@@ -10,81 +11,102 @@
 
 class Controller;
 
+/// The main interface
 class MyWindow : public yui::Win3D, public integration::IntegrableSystem {
 public:
- MyWindow(kinematics::FileInfoDof *_motion, dynamics::SkeletonDynamics* _mList = 0, ...): Win3D() {
-        mBackground[0] = 1.0;
-        mBackground[1] = 1.0;
-        mBackground[2] = 1.0;
-        mBackground[3] = 1.0;
-		
-        mSim = false;
-        mPlay = false;
-        mSimFrame = 0;
-        mPlayFrame = 0;
 
-        mPersp = 30.f;
-        mTrans[2] = -1.f;
-    
-        mGravity = Eigen::Vector3d(0.0, -9.8, 0.0);
-        mTimeStep = 1.0/1000.0;
-        mForce = Eigen::Vector3d::Zero();
+	/// The constructor
+	MyWindow(kinematics::FileInfoDof *_motion, dynamics::SkeletonDynamics* _mList = 0, ...) :
+			Win3D() {
 
-        mMotion = _motion;
-        if (_mList) {
-            mSkels.push_back(_mList);
-            va_list ap;
-            va_start(ap, _mList);
-            while (true) {
-                dynamics::SkeletonDynamics *skel = va_arg(ap, dynamics::SkeletonDynamics*);
-                if(skel)
-                    mSkels.push_back(skel);
-                else
-                    break;
-            }
-            va_end(ap);
-        }
-        
-        int sumNDofs = 0;
-        mIndices.push_back(sumNDofs);
-        for (unsigned int i = 0; i < mSkels.size(); i++) {
-            int nDofs = mSkels[i]->getNumDofs();
-            sumNDofs += nDofs;
-            mIndices.push_back(sumNDofs);
-        }
-        initDyn();
-    }
+		// Set the background color
+		mBackground[0] = 1.0;
+		mBackground[1] = 1.0;
+		mBackground[2] = 1.0;
+		mBackground[3] = 1.0;
 
-    virtual void draw();
-    virtual void keyboard(unsigned char key, int x, int y);
-    virtual void displayTimer(int _val);
+		// Set the simulator and replay options
+		mSim = false;
+		mPlay = false;
+		mSimFrame = 0;
+		mPlayFrame = 0;
 
-    // Needed for integration
-    virtual Eigen::VectorXd getState();
-    virtual Eigen::VectorXd evalDeriv();
-    virtual void setState(Eigen::VectorXd state);	
+		// Set the camera view angle and translation from the origin (OpenGL)
+		mPersp = 60.f;
+		mTrans[2] = -1.f;
 
- protected:	
-    kinematics::FileInfoDof *mMotion;
-    int mSimFrame;
-    bool mSim;
-    int mPlayFrame;
-    bool mPlay;
-    integration::EulerIntegrator mIntegrator;
-    std::vector<Eigen::VectorXd> mBakedStates;
+		// Set the simulation properties: gravity, time step and ?
+		mGravity = Eigen::Vector3d(0.0, -9.8, 0.0);
+		mTimeStep = 1.0 / 1000.0;
+		mForce = Eigen::Vector3d::Zero();
 
-    std::vector<dynamics::SkeletonDynamics*> mSkels;
-    std::vector<Eigen::VectorXd> mDofVels;
-    std::vector<Eigen::VectorXd> mDofs;
-    double mTimeStep;
-    Eigen::Vector3d mGravity;
-    Eigen::Vector3d mForce;
-    std::vector<int> mIndices;
-    Controller *mController;
+		// Initialize the motion of the skeleton
+		mMotion = _motion;
+		if(_mList) {
 
-    void initDyn();
-    void setPose();
-    void bake();
+			// Get the skeletons set in main.cpp - the hand skeleton in this case.
+			mSkels.push_back(_mList);
+			va_list ap;
+			va_start(ap, _mList);
+			while (true) {
+				dynamics::SkeletonDynamics *skel = va_arg(ap, dynamics::SkeletonDynamics*);
+				if(skel)
+					mSkels.push_back(skel);
+				else break;
+			}
+			va_end(ap);
+		}
+
+		// Get the number of degrees in the world - as indices to the state (?)
+		// Each index in mIndices creates a range for the nDofs of the skeleton it corresponds to.
+		int sumNDofs = 0;
+		mIndices.push_back(sumNDofs);
+		for (unsigned int i = 0; i < mSkels.size(); i++) {
+			int nDofs = mSkels[i]->getNumDofs();
+			sumNDofs += nDofs;
+			mIndices.push_back(sumNDofs);
+		}
+
+		// Initialize the dynamics
+		initDyn();
+	}
+
+public:
+
+	virtual void draw();
+	virtual void keyboard(unsigned char key, int x, int y);
+	virtual void displayTimer(int _val);
+
+	// Needed for integration
+	virtual Eigen::VectorXd getState();
+	virtual Eigen::VectorXd evalDeriv();
+	virtual void setState(Eigen::VectorXd state);
+
+protected:
+
+	int mSimFrame;				///< The frame the simulation is in
+	bool mSim;						///< Indicates whether the simulation is on
+	int mPlayFrame;				///< The frame the replay is in
+	bool mPlay;						///< Indicates whether the replay is on
+
+	kinematics::FileInfoDof *mMotion;							///< Describes the predefined motion of the skeleton
+	integration::EulerIntegrator mIntegrator;
+	std::vector <Eigen::VectorXd> mBakedStates;
+
+	std::vector <dynamics::SkeletonDynamics*> mSkels;			///< The skeletons in the world
+	std::vector <Eigen::VectorXd> mDofVels;								///< The velocities at dofs of each skeleton
+	std::vector <Eigen::VectorXd> mDofs;									///< The positions at dofs of each skeleton
+
+	double mTimeStep;											///< The simulation time step
+	Eigen::Vector3d mGravity;							///< The gravity in the world
+	Eigen::Vector3d mForce;								///< The force with which the finger is moved
+
+	std::vector <int> mIndices;						///< Indices to the state
+	Controller *mController;							///< The spd controller
+
+	void initDyn();
+	void setPose();
+	void bake();
 };
 
 #endif
