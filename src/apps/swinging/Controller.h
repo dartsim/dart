c52887d0e0fd81add9b09f8e847778cd07d5edfc
Can Erdogan <canerdogan.89@gmail.com>
2012-09-01 09:24:27 -0400

    Got leaning rigt; it still falls with one leg up

---
+++ src/apps/swinging/Controller.h
@@ -0,0 +1,45 @@
+#ifndef CONTROLLER_H
+#define CONTROLLER_H
+
+#include <Eigen/Dense>
+#include <iostream>
+#include <vector>
+
+#define SPD
+
+namespace dynamics{
+    class SkeletonDynamics;
+}
+
+namespace kinematics{
+    class FileInfoDof;
+}
+
+class Controller {
+ public:
+    Controller(kinematics::FileInfoDof *_motion, dynamics::SkeletonDynamics *_skel, double _t);
+    virtual ~Controller() {};
+
+    Eigen::VectorXd getTorques() { return mTorques; };
+    double getTorque(int _index) { return mTorques[_index]; };
+    void setDesiredDof(int _index, double _val) { mDesiredDofs[_index] = _val; };
+    void computeTorques(const Eigen::VectorXd& _dof, const Eigen::VectorXd& _dofVel, Eigen::VectorXd* optionalGoal = NULL);
+    dynamics::SkeletonDynamics* getSkel() { return mSkel; };
+    Eigen::VectorXd getDesiredDofs() { return mDesiredDofs; };
+    Eigen::MatrixXd getKp() {return mKp; };
+
+ protected: 
+    kinematics::FileInfoDof *mMotion;
+    dynamics::SkeletonDynamics *mSkel;
+    Eigen::VectorXd mTorques;
+    Eigen::VectorXd mDesiredDofs;
+    Eigen::MatrixXd mKp;
+    Eigen::MatrixXd mKd;
+    double mTimestep;
+    int mFrame;
+    int mInterval;
+};
+    
+    
+
+#endif // #CONTROLLER_H
