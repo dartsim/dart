cc67081e94561c9a7fc57c205186777bd5f7e18e
Saul Reynolds-Haertle <sreynoldshaertle@gmail.com>
2012-09-12 17:15:51 -0400

    Change manipulator to use new structure and screenshot

--- src/apps/manipulator/MyWindow.cpp
+++ src/apps/manipulator/MyWindow.cpp
@@ -101,163 +101,307 @@ void MyWindow::setPose() {
     mCollisionHandle->applyContactForces();
 }
 
+void MyWindow::retrieveBakedState(int frame)
+{
+    if (frame >= mBakedStates.size()) {
+        std::cout << "tried to retrieve an invalid baked state" << std::endl;
+    }
+    else{
+        for (unsigned int i = 0; i < mSkels.size(); i++) {
+            int start = mIndices[i];
+            int size = mDofs[i].size();
+            mSkels[i]->setPose(mBakedStates[frame].segment(start, size), false, false);
+        }
+    }
+}
+
 void MyWindow::displayTimer(int _val)
 {
-    int numIter = mDisplayTimeout / (mTimeStep * 1000);
-    if (mPlay) {
-        mPlayFrame += 16;
-        if (mPlayFrame >= mBakedStates.size())
-            mPlayFrame = 0;
+    std::cout << "timer" << std::endl;
+    switch(mPlayState)
+    {
+    case PLAYBACK:
+        if (mPlayFrame >= 0 && mPlayFrame >= mBakedStates.size() - mDisplayFrequency)
+            mPlayFrame = -mDisplayFrequency;
+        mPlayFrame += mDisplayFrequency;
+        retrieveBakedState(mPlayFrame);
         glutPostRedisplay();
         glutTimerFunc(mDisplayTimeout, refreshTimer, _val);      
-    }else if (mSim) {        
-        //        static Timer tSim("Simulation");
+        break;
+    case RECORD:
+        if (mScreenshotScheduled) { // Wait for every frame to be drawn and captured
+            glutPostRedisplay();
+            glutTimerFunc(mDisplayTimeout + 1000.0, refreshTimer, _val);
+        }
+        else if (mMovieFrame >= 0 && mMovieFrame >= mBakedStates.size() - mDisplayFrequency) {
+            mPlayState = PAUSED;
+        }
+        else {
+            mMovieFrame += mDisplayFrequency;
+            retrieveBakedState(mMovieFrame);
+            mScreenshotScheduled = true;
+            glutPostRedisplay();
+            glutTimerFunc(mDisplayTimeout, refreshTimer, _val);
+        }
+        break;
+    case SIMULATE:
+        int numIter = mDisplayTimeout / (mTimeStep * 1000);
+        setPose();              // retrieve current simulation state
         for (int i = 0; i < numIter; i++) {
+            // compute and apply mForce
             if (mSimFrame > 1000 && mSimFrame < 1033) {
                 cout << "push on the ball" << endl;
                 mForce[0] = 60;
             } else {
                 mForce[0] = 0.0;
             }
-            //            tSim.startTimer();
-            static_cast<BodyNodeDynamics*>(mSkels[2]->getNode("sphere_root"))->addExtForce(Vector3d(0.0, 0.0, 0), mForce);        
+            static_cast<BodyNodeDynamics*>(mSkels[2]->getNode("sphere_root"))
+                ->addExtForce(Vector3d(0.0, 0.0, 0), mForce);        
+            // compute and apply pd controller value
             mController->computeTorques(mDofs[1], mDofVels[1], mDofVels[2]);
             mSkels[1]->setInternalForces(mController->getTorques());
             mIntegrator.integrate(this, mTimeStep);
-            //            tSim.stopTimer();
-            //tSim.printScreen();
             bake();
             mSimFrame++;
         }
-        mForce.setZero();
-
         glutPostRedisplay();
         glutTimerFunc(mDisplayTimeout, refreshTimer, _val);
+        break;
     }
+}
+
+
+void MyWindow::drawText()
+{
+    // display the frame count, the playback frame, and the movie length in 2D text
+    glDisable(GL_LIGHTING);
+    glColor3f(0.0,0.0,0.0);
+    char buff[128];
+    string frame;
+
+    switch(mPlayState)
+    {
+    case PAUSED:
+        sprintf(buff," ");
+        break;
+    case PLAYBACK:
+        sprintf(buff,"Playing");
+        break;
+    case SIMULATE:
+        sprintf(buff,"Simulating");
+        break;
+    case RECORD:
+        sprintf(buff,"Saving a Movie");
+        break;
+    }
+    frame = string(buff);
+    yui::drawStringOnScreen(0.02f,0.17f,frame);
+
+    sprintf(buff,"Sim Frame: %d", mSimFrame);
+    frame = string(buff);
+    yui::drawStringOnScreen(0.02f,0.12f,frame);
+
+    sprintf(buff,"Play Frame: %d", mPlayFrame);
+    frame = string(buff);
+    yui::drawStringOnScreen(0.02f,0.07f,frame);
+
+    sprintf(buff,"Movie Frame: %d", mMovieFrame);
+    frame = string(buff);
+    yui::drawStringOnScreen(0.02f,0.02f,frame);
+
+    glEnable(GL_LIGHTING);
+}
+
+void MyWindow::drawContact(Vector3d vertex, Vector3d force, Vector3d penColor1, Vector3d penColor2, Vector3d ellipsoidColor)
+{
+    mRI->setPenColor(penColor1);
+    glBegin(GL_LINES);
+    glVertex3f(vertex[0], vertex[1], vertex[2]);
+    glVertex3f(vertex[0] + force[0], vertex[1] + force[1], vertex[2] + force[2]);
+    glEnd();
+    mRI->setPenColor(penColor2);
+    mRI->pushMatrix();
+    glTranslated(vertex[0], vertex[1], vertex[2]);
+    mRI->drawEllipsoid(ellipsoidColor);
+    mRI->popMatrix();
 }
 
 void MyWindow::draw()
 {
+    std::cout << "draw" << std::endl;
+
     glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
 
-    if (!mSim) {
-        if (mPlayFrame < mBakedStates.size()) {
-            for (unsigned int i = 0; i < mSkels.size(); i++) {
-                int start = mIndices[i];
-                int size = mDofs[i].size();
-                mSkels[i]->setPose(mBakedStates[mPlayFrame].segment(start, size), false, false);
+    // draw contact points and associated force vectors
+    if (mShowMarkers) {
+        Vector3d penColor1;
+        Vector3d penColor2;
+        Vector3d ellipsoidColor;
+
+        playstate_enum disp = mPlayState;
+        if (mPlayState == PAUSED) disp = mPlayStateLast;
+
+        if (disp == SIMULATE) {
+            penColor1 = Vector3d(0.2, 0.2, 0.8);
+            penColor2 = Vector3d(0.2, 0.8, 0.2);
+            ellipsoidColor = Vector3d(0.01, 0.01, 0.01);
+            for (int k = 0; k < mCollisionHandle->getCollisionChecker()->getNumContact(); k++) {
+                drawContact(mCollisionHandle->getCollisionChecker()->getContact(k).point, 
+                            mCollisionHandle->getCollisionChecker()->getContact(k).force / 100.0,
+                            penColor1,
+                            penColor2,
+                            ellipsoidColor);
             }
-            if (mShowMarkers) {
+        }
+        else { // disp == PLAYBACK || disp == RECORD
+            int frame;
+            penColor1 = Vector3d(0.2, 0.2, 0.8);
+            penColor2 = Vector3d(0.2, 0.8, 0.2);
+            ellipsoidColor = Vector3d(0.01, 0.01, 0.01);
+            if (disp == RECORD) frame = mMovieFrame;
+            if (disp == PLAYBACK) frame = mPlayFrame;
+            if (frame < 0 || frame >= mBakedStates.size())
+            {
+                cout << "invalid frame during draw" << disp << " " << RECORD << " " << PLAYBACK << endl;
+                cout << frame << " >= " << mBakedStates.size() << endl;
+            }
+            else
+            {
                 int sumDofs = mIndices[mSkels.size()]; 
-                int nContact = (mBakedStates[mPlayFrame].size() - sumDofs) / 6;
-                for (int i = 0; i < nContact; i++) {
-                    Vector3d v = mBakedStates[mPlayFrame].segment(sumDofs + i * 6, 3);
-                    Vector3d f = mBakedStates[mPlayFrame].segment(sumDofs + i * 6 + 3, 3) / 10.0;
-                    glBegin(GL_LINES);
-                    glVertex3f(v[0], v[1], v[2]);
-                    glVertex3f(v[0] + f[0], v[1] + f[1], v[2] + f[2]);
-                    glEnd();
-                    mRI->setPenColor(Vector3d(0.2, 0.8, 0.2));
-                    mRI->pushMatrix();
-                    glTranslated(v[0], v[1], v[2]);
-                    mRI->drawEllipsoid(Vector3d(0.01, 0.01, 0.01));
-                    mRI->popMatrix();
+                int nContact = (mBakedStates[frame].size() - sumDofs) / 6;
+                for (int k = 0; k < nContact; k++) {
+                    drawContact(mBakedStates[frame].segment(sumDofs + k * 6, 3),
+                                mBakedStates[frame].segment(sumDofs + k * 6 + 3, 3) / 10.0,
+                                penColor1,
+                                penColor2,
+                                ellipsoidColor);
                 }
             }
         }
-    }else{
-        if (mShowMarkers) {
-            for (int k = 0; k < mCollisionHandle->getCollisionChecker()->getNumContact(); k++) {
-                Vector3d  v = mCollisionHandle->getCollisionChecker()->getContact(k).point;
-                Vector3d n = mCollisionHandle->getCollisionChecker()->getContact(k).normal / 10.0;
-                Vector3d f = mCollisionHandle->getCollisionChecker()->getContact(k).force / 100.0;
-
-                mRI->setPenColor(Vector3d(0.2, 0.2, 0.8));
-                glBegin(GL_LINES);
-                glVertex3f(v[0], v[1], v[2]);
-                glVertex3f(v[0] + f[0], v[1] + f[1], v[2] + f[2]);
-                glEnd();
-                mRI->setPenColor(Vector3d(0.2, 0.8, 0.2));
-                mRI->pushMatrix();
-                glTranslated(v[0], v[1], v[2]);
-                mRI->drawEllipsoid(Vector3d(0.01, 0.01, 0.01));
-                mRI->popMatrix();
-            }
-        }
     }
-
     
     for (unsigned int i = 0; i < mSkels.size(); i++)
         mSkels[i]->draw(mRI);
-            
-    // display the frame count in 2D text
-    glDisable(GL_LIGHTING);
-    char buff[64];
-    if (!mSim) 
-        sprintf(buff, "%d", mPlayFrame);
-    else
-        sprintf(buff, "%d", mSimFrame);
-    string frame(buff);
-    glColor3f(0.0,0.0,0.0);
-    yui::drawStringOnScreen(0.02f,0.02f,frame);
-    glEnable(GL_LIGHTING);
+    
+    // take screenshots before putting up text
+    if (mScreenshotScheduled)
+    {
+        mScreenshotScheduled = false;
+        screenshot();
+    }
+    
+    drawText();
 }
 
 void MyWindow::keyboard(unsigned char key, int x, int y)
 {
     switch(key){
-    case ' ': // use space key to play or stop the motion
-        mSim = !mSim;
-        if (mSim) {
-            mPlay = false;
-            glutTimerFunc( mDisplayTimeout, refreshTimer, 0);
+    case ' ': // pause or unpause whatever's running; if space is the
+              // first thing that's pressed, simulate
+        if (mPlayState == PAUSED)
+        {
+            if (mPlayStateLast != PAUSED)
+                mPlayState = mPlayStateLast;
+            else
+                mPlayState = SIMULATE;
+            glutTimerFunc(mDisplayTimeout, refreshTimer, 0);
+        }
+        else
+        {
+            mPlayStateLast = mPlayState;
+            mPlayState = PAUSED;
         }
         break;
-    case 's': // simulate one frame
-        if (!mPlay) {
-            mForce = Vector3d::Zero();
-            setPose();
-            mIntegrator.integrate(this, mTimeStep);
-            mSimFrame++;
-            bake();
+    case 's': // simulation mode
+        if (mPlayState == SIMULATE) {
+            mPlayState = PAUSED;
+        }
+        else {
+            if (mPlayState == PAUSED)
+                glutTimerFunc(mDisplayTimeout, refreshTimer, 0);
+            mPlayState = SIMULATE;
+            mPlayStateLast = SIMULATE;
+        }
+        break;
+    case 'p': // switch to playback mode
+        if (mPlayState == PLAYBACK) {
+            mPlayState = PAUSED;
+        }
+        else {
+            if (mPlayState == PAUSED)
+                glutTimerFunc(mDisplayTimeout, refreshTimer, 0);
+            mPlayState = PLAYBACK;
+            mPlayStateLast = PLAYBACK;
+        }
+        break;
+    case 'm': // switch to movie-saving mode
+        if (mPlayState == RECORD) {
+            mPlayState = PAUSED;
+        }
+        else {
+            if (mPlayState == PAUSED)
+                glutTimerFunc( mDisplayTimeout, refreshTimer, 0);
+            mPlayState = RECORD;
+            mPlayStateLast = RECORD;
+        }
+        break;
+
+
+    case 'n': // save a single screeNshot
+        screenshot();
+        break;
+    case 'f': // simulation mode, pause, and simulate one frame
+        mPlayState = PAUSED;
+        mPlayStateLast = SIMULATE;
+        
+        setPose();
+        mForce = Vector3d::Zero();
+        mIntegrator.integrate(this, mTimeStep);
+        mSimFrame++;
+        bake();
+        glutPostRedisplay();
+        break;
+    case 'r': // set playback to the first frame
+        mPlayFrame = 0;
+        retrieveBakedState(mPlayFrame);
+        glutPostRedisplay();
+        break;
+    case 't': // set playback motion to the newest simulated frame
+        mPlayFrame = mBakedStates.size()-1;
+        retrieveBakedState(mPlayFrame);
+        glutPostRedisplay();
+        break;
+    case '[':
+        if (mPlayState == PLAYBACK || mPlayStateLast == PLAYBACK)
+        {
+            mPlayFrame -= 1;
+            if (mPlayFrame < 0) mPlayFrame = mBakedStates.size();
+            retrieveBakedState(mPlayFrame);
             glutPostRedisplay();
         }
         break;
-    case '1':
-        mForce[2] = 40;
-        cout << "push" << endl;
-        break;
-    case '2':
-        mForce[2] = -40;
-        cout << "push" << endl;
-        break;
-    case 'p': // playBack
-        mPlay = !mPlay;
-        if (mPlay) {
-            mSim = false;
-            glutTimerFunc( mDisplayTimeout, refreshTimer, 0);
-        }
-        break;
-    case '[': // step backward
-        if (!mSim) {
-            mPlayFrame--;
-            if(mPlayFrame < 0)
-                mPlayFrame = 0;
-            glutPostRedisplay();
-        }
-        break;
-    case ']': // step forwardward
-        if (!mSim) {
-            mPlayFrame++;
-            if(mPlayFrame >= mBakedStates.size())
-                mPlayFrame = 0;
+    case ']':
+        if (mPlayState == PLAYBACK || mPlayStateLast == PLAYBACK)
+        {
+            mPlayFrame += 1;
+            if (mPlayFrame >= mBakedStates.size()) mPlayFrame = 0;
+            retrieveBakedState(mPlayFrame);
             glutPostRedisplay();
         }
         break;
     case 'v': // show or hide markers
         mShowMarkers = !mShowMarkers;
         break;
- 
+
+    case '1': // upper right force
+        mForce[0] = 40;
+        cout << "push up and right" << endl;
+        break;
+    case '2': // upper right force
+        mForce[0] = -40;
+        cout << "push left" << endl;
+        break;
+
     default:
         Win3D::keyboard(key,x,y);
     }
