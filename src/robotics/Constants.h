0ddc92feffad17542ffb1a87a359af0923ed2fae
Ana Huaman <ahuaman3@gatech.edu>
2012-10-12 13:00:09 -0400

    The 6 DOF for robot origin are now optional

--- src/robotics/Constants.h
+++ src/robotics/Constants.h
@@ -1,5 +1,5 @@
-#ifndef	__ROBOTICS_CONSTANTS_H__
-#define	__ROBOTICS_CONSTANTS_H__
+#ifndef	CONSTANTS_H
+#define CONSTANTS_H
 
 #include <math.h>
 #include <float.h>
@@ -26,4 +26,4 @@ inline double RAD2DEG(int r)	 { return (r * DEGREE); }
 inline double DEG2RAD(double d)  { return (d * RADIAN); }
 inline double RAD2DEG(double r)	 { return (r * DEGREE); }
 
-#endif /** __ROBOTICS_CONSTANTS_H__ */
+#endif /** CONSTANTS_H */
