9755465d72c076f58f3c0845f5f932aad76739f4
pushkar <pushkar@cc>
2013-01-05 04:05:17 -0500

    Adds a help script for merging, uploading website

---
+++ auto.sh
@@ -0,0 +1,67 @@
+#!/bin/sh
+
+PRG=$0
+usage()
+{
+    echo "usage: dart [<args>]" 
+    echo ""
+    echo "The most commonly used commands are:"
+    echo "  merge_graphics  Merges the graphics branch into master"
+    echo "  merge_robotics  Merges the robotics branch into master"
+    echo "  generate_debs   Generates debian on 32 or 64 bit machines"
+    echo "  generate_docs   Generates website documentation"
+    echo "  upload_web      Uploads the new website"
+    echo ""
+    exit 1
+}
+
+debs()
+{
+MACHINE_TYPE=`uname -m`
+if [ ${MACHINE_TYPE} == 'x86_64' ]; then
+  cpack -D CPACK_SYSTEM_NAME=amd64 -D CPACK_DEBIAN_PACKAGE_ARCHITECTURE=amd64 -G DEB
+else
+  cpack -D CPACK_SYSTEM_NAME=i386 -D CPACK_DEBIAN_PACKAGE_ARCHITECTURE=i386 -G DEB
+fi
+}
+
+web()
+{
+cd docs
+tar czf html.tar.gz html
+scp html.tar.gz golems@dart.golems.org:~/
+ssh golems@golems.org 'tar -xvf html.tar.gz'
+}
+
+[ "$#" -lt 1 ] && usage
+
+# parse commandline
+while [ $# -gt 0 ]
+do
+      arg="$1"
+      case "$arg" in
+         merge_graphics) 
+         git checkout master
+         git merge graphics
+         echo "You are now checked out in the master branch"
+         ;;
+         merge_robotics) 
+         git checkout master
+         git merge robotics
+         echo "You are now checked out in the master branch"
+         ;;
+         generate_debs)
+         debs    
+         ;;
+         generate_docs)
+         make docs
+         ;;
+         upload_web)
+         web
+         ;;
+         --) shift; break;;  # no more options
+         -*) usage ;; 
+         *) break;; # not option, its some argument
+       esac
+       shift
+done
