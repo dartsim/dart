cc58fa2a9a04eb169453ad11dcbfe1cc44478e2b
Pushkar Kolhe <pushkarkolhe@gmail.com>
2013-01-09 18:38:32 -0500

    Removes libccd and fcl from thirdparty. Requires you to install them from pre-built binaries

--- thirdparty/fcl/CMakeModules/FCLVersion.cmake
+++
@@ -1,8 +0,0 @@
-# set the version in a way CMake can use
-set(FCL_MAJOR_VERSION 0)
-set(FCL_MINOR_VERSION 2)
-set(FCL_PATCH_VERSION 3)
-set(FCL_VERSION "${FCL_MAJOR_VERSION}.${FCL_MINOR_VERSION}.${FCL_PATCH_VERSION}")
-
-# increment this when we have ABI changes
-set(FCL_ABI_VERSION 1)
