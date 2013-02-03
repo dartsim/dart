fee6f73266802b55ab97db1bb2eaf847a97a98a6
Can Erdogan <canerdogan.89@gmail.com>
2013-02-03 08:10:11 -0500

    Added an example construction of a skeleton through code (not model files)

---
+++ apps/skeleton/MyWindow.cpp
@@ -0,0 +1,43 @@
+/**
+ * @file MyWindow.h
+ * @author Can Erdogan
+ * @date Feb 02, 2013
+ * @brief Simple example of a skeleton created from scratch.
+ */
+
+#include "MyWindow.h"
+
+using namespace Eigen;
+using namespace kinematics;
+using namespace dynamics;
+
+void MyWindow::draw() {
+	glDisable (GL_LIGHTING);
+	glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+	glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+	skel->draw(mRI);
+}
+
+void MyWindow::keyboard(unsigned char key, int x, int y) {
+  static bool inverse = false;
+	static const double dDOF = 0.1;
+	switch(key) {
+		case '-': {
+			inverse = true;
+		}	break;
+		case '1':
+		case '2': {
+			size_t dofIdx = key - 49;
+			Eigen::VectorXd pose = skel->getPose();
+			pose(dofIdx) = pose(dofIdx) + (inverse ? -dDOF : dDOF);
+			skel->setPose(pose);
+			std::cout << "Updated pose DOF " << dofIdx << ": " << pose.transpose() << std::endl;
+			glutPostRedisplay();
+			if(inverse) inverse = false;
+		}	break;
+		default:
+			Win3D::keyboard(key, x, y);
+	}
+	glutPostRedisplay();
+}
+
