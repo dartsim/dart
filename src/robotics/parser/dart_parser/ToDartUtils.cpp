19303f2ebba3d0aeb5984532ccfa737181f8b9bd
ana-GT <ahuaman3@gatech.edu>
2013-05-22 11:54:50 -0400

    Corrected debug message for NULL shapes

--- src/robotics/parser/dart_parser/ToDartUtils.cpp
+++ src/robotics/parser/dart_parser/ToDartUtils.cpp
@@ -293,17 +293,17 @@ dynamics::BodyNodeDynamics* DartLoader::createDartNode( boost::shared_ptr<urdf::
     // Set NULL
     kinematics::Shape* shape = NULL;
     node->setVisualizationShape( shape );
-    if(debug) {  std::cout<< "No Visualization element defined for node "<<node->getName() <<". Using default empty VizShape"<<std::endl; }
+    if(debug) {  std::cout<< "No Visualization tag defined for node "<<node->getName() <<". Using NULL VizShape"<<std::endl; }
   }
   // Set collision information
   if( _lk->collision ) {
     if( add_ColShape( node, _lk->collision, _rootToSkelPath ) == false ) {  std::cout<< "Error loading ColShape" <<std::endl; return NULL; }
   }
   else {
-    // Add default
+    // Set NULL
     kinematics::Shape* shape = NULL;
     node->setCollisionShape( shape );
-    if(debug) { std::cout<< "No Collision element defined for node "<<node->getName() <<". Using default empty ColShape"<<std::endl; }
+    if(debug) { std::cout<< "No Collision tag defined for node "<<node->getName() <<". Using NULL ColShape"<<std::endl; }
   }
 
   return node;
