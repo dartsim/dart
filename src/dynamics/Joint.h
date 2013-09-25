dartsim/dart

commit 11aad6bd4975848e1306984c962ed910b1589e9b

Make Joint::updateTransform() and Joint::updateVelocity() protected
Body node automatically calls these functions when necessary.

TODO: Make Joint::updateAcceleration() protected too.

src/dynamics/Joint.h
@@ -52,6 +52,8 @@ class BodyNode;
 class Joint : public GenCoordSystem
 {
 public:
+    friend BodyNode;
+
     //--------------------------------------------------------------------------
     // Types
     //--------------------------------------------------------------------------
@@ -154,14 +156,6 @@ public:
     //--------------------------------------------------------------------------
     // Recursive Kinematics Algorithms
     //--------------------------------------------------------------------------
-    /// @brief
-    /// q --> T(q)
-    virtual void updateTransform() = 0;
-
-    /// @brief
-    /// q, dq --> S(q), V(q, dq)
-    /// V(q, dq) = S(q) * dq
-    virtual void updateVelocity() = 0;
 
     /// @brief
     /// dq, ddq, S(q) --> dS(q), dV(q, dq, ddq)
@@ -183,6 +177,15 @@ public:
     void applyGLTransform(renderer::RenderInterface* _ri);
 
 protected:
+    /// @brief
+    /// q --> T(q)
+    virtual void updateTransform() = 0;
+
+    /// @brief
+    /// q, dq --> S(q), V(q, dq)
+    /// V(q, dq) = S(q) * dq
+    virtual void updateVelocity() = 0;
+
     //--------------------------------------------------------------------------
     //
     //--------------------------------------------------------------------------

src/dynamics/BodyNode.cpp
@@ -431,6 +431,8 @@ void BodyNode::drawMarkers(renderer::RenderInterface* _ri,
 
 void BodyNode::updateTransform()
 {
+    mParentJoint->updateTransform();
+
     if (mParentBodyNode)
     {
         mW = mParentBodyNode->getWorldTransform()
@@ -451,6 +453,8 @@ void BodyNode::updateVelocity(bool _updateJacobian)
     //
     // V(i) = Ad(T(i, i-1), V(i-1)) + S * dq
     //--------------------------------------------------------------------------
+
+    mParentJoint->updateVelocity();
 
     if (mParentBodyNode)
     {

src/dynamics/Skeleton.cpp
@@ -299,12 +299,11 @@ void Skeleton::updateForwardKinematics(bool _firstDerivative,
     for (std::vector<BodyNode*>::iterator itrBody = mBodyNodes.begin();
          itrBody != mBodyNodes.end(); ++itrBody)
     {
-        (*itrBody)->getParentJoint()->updateTransform();
         (*itrBody)->updateTransform();
 
-        if (_firstDerivative)
-            (*itrBody)->getParentJoint()->updateVelocity();
+        if (_firstDerivative) {
             (*itrBody)->updateVelocity();
+        }
 
         if (_secondDerivative)
         {

commit bf7c191cc77eb0b4b95d4d9bd854f27499dcda4a

Fix compiler error in GCC caused by 11aad6bd4975848e1306984c962ed910b1589e9b

src/dynamics/Joint.h
@@ -52,7 +52,7 @@ class BodyNode;
 class Joint : public GenCoordSystem
 {
 public:
-    friend BodyNode;
+    friend class BodyNode;
 
     //--------------------------------------------------------------------------
     // Types