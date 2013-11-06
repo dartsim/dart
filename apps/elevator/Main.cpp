71db2b74c8df314ae614cd83a914bf7c50e84297
Karen Liu <ckarenliu@gmail.com>
2013-11-06 16:20:38 -0500

    project 4 done

--- apps/elevator/Main.cpp
+++ apps/elevator/Main.cpp
@@ -56,6 +56,16 @@ int main(int argc, char* argv[])
   
     glutInit(&argc, argv);
     window.initWindow(640, 480, "This is going to hurt!");
+    std::cout << "space bar: simulation on/off" << std::endl;
+    std::cout << "'p': playback/stop" << std::endl;
+    std::cout << "'[' and ']': play one frame backward and forward" << std::endl;
+    std::cout << "'v': contact visualization on/off" << std::endl;
+    std::cout << "Left click: rotate camera" << std::endl;
+    std::cout << "Right click: pan camera" << std::endl;
+    std::cout << "Shift + Left click: zoom camera" << std::endl;
+    std::cout << "Press 'l' to use jet pack" << std::endl;
+    std::cout << "Press '-/=' to make the walls sturdier or flimsier. This is just for fun." << std::endl;
+    
     glutMainLoop();
 
     return 0;
