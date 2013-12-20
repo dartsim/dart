9a50f02697b7b68846685b9a6603cf89aaf51d7d
Jeongseok Lee (JS) <jslee02@gmail.com>
2013-12-20 14:30:43 -0500

    Clean up FreeJoint.cpp

--- dart/dynamics/FreeJoint.cpp
+++ dart/dynamics/FreeJoint.cpp
@@ -66,13 +66,6 @@ FreeJoint::~FreeJoint() {
 }
 
 void FreeJoint::updateTransform() {
-  Eigen::Vector3d q1(mCoordinate[0].get_q(),
-      mCoordinate[1].get_q(),
-      mCoordinate[2].get_q());
-  Eigen::Vector3d q2(mCoordinate[3].get_q(),
-      mCoordinate[4].get_q(),
-      mCoordinate[5].get_q());
-
   // TODO(JS): This is workaround for Issue #122.
   mT_Joint = math::expMap(get_q());
 
@@ -93,8 +86,8 @@ void FreeJoint::updateTransform_Issue122(double _timeStep) {
 
 void FreeJoint::updateJacobian() {
   Eigen::Vector3d q(mCoordinate[0].get_q(),
-      mCoordinate[1].get_q(),
-      mCoordinate[2].get_q());
+                    mCoordinate[1].get_q(),
+                    mCoordinate[2].get_q());
 
   Eigen::Matrix3d J = math::expMapJac(q);
 
@@ -149,11 +142,11 @@ void FreeJoint::updateJacobian_Issue122() {
 
 void FreeJoint::updateJacobianTimeDeriv() {
   Eigen::Vector3d q(mCoordinate[0].get_q(),
-      mCoordinate[1].get_q(),
-      mCoordinate[2].get_q());
+                    mCoordinate[1].get_q(),
+                    mCoordinate[2].get_q());
   Eigen::Vector3d dq(mCoordinate[0].get_dq(),
-      mCoordinate[1].get_dq(),
-      mCoordinate[2].get_dq());
+                     mCoordinate[1].get_dq(),
+                     mCoordinate[2].get_dq());
 
   Eigen::Matrix3d dJ = math::expMapJacDot(q, dq);
 
