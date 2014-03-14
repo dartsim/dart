f8b54cd2fbdf76ef8da51e173dfaae4675e14e8d
Jeongseok Lee <jslee02@gmail.com>
2014-03-14 10:21:56 -0400

    Try to build nlopt in Travis-CI

--- ci/before_install.sh
+++ ci/before_install.sh
@@ -1,3 +1,12 @@
+before_install() {
+  cd /tmp
+
+  # Install nlopt from source since Ubuntu 12.04 does not provide debian package for nlopt
+  curl -o nlopt-2.4.1.tar.gz http://ab-initio.mit.edu/nlopt/nlopt-2.4.1.tar.gz
+  tar -xf nlopt-2.4.1.tar.gz
+  (cd nlopt-2.4.1/; sh autogen.sh; make && sudo make install)
+}
+
 sudo add-apt-repository --yes ppa:libccd-debs/ppa
 sudo add-apt-repository --yes ppa:fcl-debs/ppa
 sudo add-apt-repository --yes ppa:dartsim/ppa
@@ -31,10 +40,5 @@ else
   sudo apt-get --yes --force-yes install $APT_CORE
 fi
 
-# Install nlopt from source since Ubuntu 12.04 does not provide debian package for nlopt
-curl -o nlopt-2.4.1.tar.gz http://ab-initio.mit.edu/nlopt/nlopt-2.4.1.tar.gz
-tar -xf nlopt-2.4.1.tar.gz
-cd nlopt-2.4.1/
-sh autogen.sh
-make && sudo make install
+(before_install)
 
