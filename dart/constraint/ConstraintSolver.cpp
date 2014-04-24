8d4e154dc1413d065be1599478fc3e122d738991
Jeongseok Lee <jslee02@gmail.com>
2014-04-23 21:33:51 -0400

    Use DanzigLCPSolver per default

--- dart/constraint/ConstraintSolver.cpp
+++ dart/constraint/ConstraintSolver.cpp
@@ -62,7 +62,7 @@ using namespace dynamics;
 ConstraintSolver::ConstraintSolver(double _timeStep)
   : mTimeStep(_timeStep),
     mCollisionDetector(new collision::FCLMeshCollisionDetector()),
-    mLCPSolver(new PGSLCPSolver(mTimeStep))
+    mLCPSolver(new DantzigLCPSolver(mTimeStep))
 {
   assert(_timeStep > 0.0);
 }
