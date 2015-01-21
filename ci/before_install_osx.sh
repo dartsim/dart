ecf3b723792e95048f2ca064eeb49c9a68f158fe
Jeongseok Lee <jslee02@gmail.com>
2015-01-21 06:23:06 -0500

    [Travis-CI] Fix location of homebrew urdfdom on osx

--- ci/before_install_osx.sh
+++ ci/before_install_osx.sh
@@ -2,15 +2,15 @@ brew tap dartsim/dart
 brew tap homebrew/science
 
 brew install git
-brew install cmake
-brew install flann
-brew install boost
-brew install eigen
-brew install tinyxml
-brew install tinyxml2
-brew install libccd
-brew install nlopt
-brew install urdfdom
-brew install urdfdom_headers
-brew install console_bridge
+# brew install cmake  # installed cmake-3.0.2
+brew install flann  # 1.8.14
+# brew install boost  # installed boost-1.55.2
+brew install eigen  # 3.2.2
+brew install tinyxml  # 2.6.2
+brew install tinyxml2  # 2.2.0
+brew install libccd  #
+brew install nlopt  # 2.4.2
+brew install ros/deps/urdfdom
+brew install ros/deps/urdfdom_headers
+brew install ros/deps/console_bridge
 
