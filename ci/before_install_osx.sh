ec7d1be1f92807251bc7135f1ebf94e1fabc671f
Jeongseok Lee <jslee02@gmail.com>
2015-01-21 05:09:36 -0500

    [Travis-CI] Testing build on osx with homebrew

---
+++ ci/before_install_osx.sh
@@ -0,0 +1,12 @@
+brew tap dartsim/dart
+brew tap homebrew/science
+
+brew install git
+brew install cmake
+brew install flann
+brew install boost
+brew install eigen
+brew install tinyxml
+brew install tinyxml2
+brew install libccd
+brew install nlopt
