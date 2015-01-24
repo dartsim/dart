4c8b8ad8ac79f958c7ebc10a0f6185f87086ae67
Jeongseok Lee <jslee02@gmail.com>
2015-01-24 12:08:49 -0500

    [Travis-CI] Enable unit tests on Mac

--- ci/script_osx.sh
+++ ci/script_osx.sh
@@ -1,5 +1,5 @@
 make
-# if [ $BUILD_CORE_ONLY = OFF ]; then make test; fi  # Disabled until gtest issue is resolved
+if [ $BUILD_CORE_ONLY = OFF ]; then make test; fi
 # cd tools/
 # ./code_check.sh
 # ./abi_check.sh 4.1.0  # Check with DART 4.1.0
