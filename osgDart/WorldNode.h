0262a2ff48483d24e1aff71a281af97d4fe6c94a
M.X. Grey <mxgrey@gatech.edu>
2015-02-17 22:15:05 -0500

    setting up osgDart

---
+++ osgDart/WorldNode.h
@@ -0,0 +1,15 @@
+#ifndef OSGDART_WORLDNODE_H
+#define OSGDART_WORLDNODE_H
+
+namespace osgDart
+{
+
+/// WorldNode class encapsulates a World to be displayed in OpenSceneGraph
+class WorldNode
+{
+
+};
+
+} // namespace osgDart
+
+#endif // OSGDART_WORLDNODE_H
