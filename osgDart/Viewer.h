0b31f2e73f6a7a06f08bbd658d73d3e327daa515
M.X. Grey <mxgrey@gatech.edu>
2015-02-27 03:55:29 -0500

    experimenting with lighting

--- osgDart/Viewer.h
+++ osgDart/Viewer.h
@@ -1,4 +1,77 @@
-#ifndef VIEWER_H
-#define VIEWER_H
+/*
+ * Copyright (c) 2015, Georgia Tech Research Corporation
+ * All rights reserved.
+ *
+ * Author(s): Michael X. Grey <mxgrey@gatech.edu>
+ *
+ * Georgia Tech Graphics Lab and Humanoid Robotics Lab
+ *
+ * Directed by Prof. C. Karen Liu and Prof. Mike Stilman
+ * <karenliu@cc.gatech.edu> <mstilman@cc.gatech.edu>
+ *
+ * This file is provided under the following "BSD-style" License:
+ *   Redistribution and use in source and binary forms, with or
+ *   without modification, are permitted provided that the following
+ *   conditions are met:
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above
+ *     copyright notice, this list of conditions and the following
+ *     disclaimer in the documentation and/or other materials provided
+ *     with the distribution.
+ *   THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND
+ *   CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
+ *   INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ *   MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ *   DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
+ *   CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ *   SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ *   LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
+ *   USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+ *   AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ *   LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ *   ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ *   POSSIBILITY OF SUCH DAMAGE.
+ */
 
-#endif // VIEWER_H
+#ifndef OSGDART_VIEWER_H
+#define OSGDART_VIEWER_H
+
+#include <osgViewer/Viewer>
+
+namespace osgDart
+{
+
+class WorldNodeEventHandler;
+
+class Viewer : public osgViewer::Viewer
+{
+public:
+
+  /// Constructor for osgDart::Viewer. This will automatically create the
+  /// default event handlers (such as WorldNodeEventHandler). To clear these,
+  /// use clearDefaultEventHandlers();
+  Viewer();
+
+  /// Destructor
+  virtual ~Viewer();
+
+  /// Removes all the default event handlers that come with osgDart::Viewer
+  virtual void clearDefaultEventHandlers();
+
+  /// Removes the default WorldNodeEventHandler
+  void clearWorldNodeEventHandler();
+
+  /// Pass in true to turn headlights on, false to turn headlights off
+  void switchHeadlights(bool _on);
+
+protected:
+
+  /// Default WorldNodeEventHandler for this osgDart::Viewer
+  WorldNodeEventHandler* mWorldNodeEventHandler;
+
+};
+
+}
+
+#endif // OSGDART_VIEWER_H
