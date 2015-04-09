a5e211244f59ee28e32958f5e55e3533a301a9a9
Karen Liu <ckarenliu@gmail.com>
2015-04-08 22:39:22 -0400

    moved glutInit before the construction of MyWindow in main.cpp

--- apps/twister/Main.cpp
+++ apps/twister/Main.cpp
@@ -4,17 +4,16 @@
 
 int main(int argc, char* argv[]) 
 {
-
-    // Create a window for rendering and UI
-    MyWindow window;
-
     // Create a world
     MyWorld *world = new MyWorld();
 
     // Link the world to the window and start running the glut event loop
-    window.setWorld(world);
+    // Create a window for rendering and UI
     glutInit(&argc, argv);
+    MyWindow window;
     window.initWindow(640, 480, "Twister");
+    window.setWorld(world);
+
     std::cout << "Alt + Left click: grab a marker" << std::endl;
     std::cout << "Alt + Right click: remove a constraint" << std::endl;
     std::cout << "'v': visualization on/off" << std::endl;
