027c2a1a12f0fa441b7fc19f178eb223d8c4ced3
M.X. Grey <mxgrey@gatech.edu>
2015-06-15 22:30:15 -0400

    adding damping to the tutorial

--- tutorials/tutorialMultiPendulum.cpp
+++ tutorials/tutorialMultiPendulum.cpp
@@ -43,6 +43,8 @@ constexpr double default_depth = 0.2;  // m
 constexpr double default_torque = 15.0; // N-m
 constexpr int default_countdown = 200; // Number of timesteps for applying force
 
+constexpr double default_damping = 0.1;
+
 using namespace dart::dynamics;
 using namespace dart::simulation;
 
@@ -119,11 +121,20 @@ public:
 
   void timeStepping() override
   {
-    for(size_t i=0; i<_forceCountDown.size(); ++i)
+    // Reset all the shapes to be Blue
+    for(size_t i=0; i<_pendulum->getNumBodyNodes(); ++i)
     {
       const ShapePtr& shape =
           _pendulum->getBodyNode(i)->getVisualizationShape(0);
+
+      shape->setColor(dart::Color::Blue());
+    }
+
+    // Any DOFs with an active countdown are given a joint force and colored red
+    for(size_t i=0; i<_forceCountDown.size(); ++i)
+    {
       DegreeOfFreedom* dof = _pendulum->getDof(i);
+      const ShapePtr& shape = dof->getChildBodyNode()->getVisualizationShape(0);
 
       if(_forceCountDown[i] > 0)
       {
@@ -131,13 +142,9 @@ public:
         shape->setColor(dart::Color::Red());
         --_forceCountDown[i];
       }
-      else
-      {
-        dof->setForce(0.0);
-        shape->setColor(dart::Color::Blue());
-      }
     }
 
+    // Step the simulation forward
     SimWindow::timeStepping();
   }
 
@@ -145,6 +152,11 @@ public:
   {
     if(index < _forceCountDown.size())
       _forceCountDown[index] = default_countdown;
+  }
+
+  void changeDamping(double delta)
+  {
+
   }
 
   /// Add a constraint to turn the bottom of the pendulum into a triangle
@@ -181,23 +193,8 @@ protected:
   bool _positiveSign;
 };
 
-BodyNode* addBody(const SkeletonPtr& pendulum,
-             BodyNode* parent, const std::string& name)
+void setGeometry(const BodyNodePtr& bn)
 {
-  // Set up the properties for the Joint
-  RevoluteJoint::Properties properties;
-  properties.mName = name+"_joint";
-  properties.mAxis = Eigen::Vector3d::UnitY();
-  if(parent)
-  {
-    properties.mT_ParentBodyToJoint.translation() =
-        Eigen::Vector3d(0, 0, default_height);
-  }
-
-  // Create a new BodyNode, attached to its parent by a RevoluteJoint
-  BodyNodePtr bn = pendulum->createJointAndBodyNodePair<RevoluteJoint>(
-        parent, properties, BodyNode::Properties(name)).second;
-
   // Create a BoxShape to be used for both visualization and collision checking
   std::shared_ptr<BoxShape> box(new BoxShape(
       Eigen::Vector3d(default_width, default_depth, default_height)));
@@ -209,11 +206,68 @@ BodyNode* addBody(const SkeletonPtr& pendulum,
   box_tf.translation() = center;
   box->setLocalTransform(box_tf);
 
+  // Add it as a visualization and collision shape
   bn->addVisualizationShape(box);
   bn->addCollisionShape(box);
 
   // Move the center of mass to the center of the object
   bn->setLocalCOM(center);
+}
+
+BodyNode* makeRootBody(const SkeletonPtr& pendulum, const std::string& name)
+{
+  BallJoint::Properties properties;
+  properties.mName = name+"_joint";
+  properties.mDampingCoefficients = Eigen::Vector3d::Constant(default_damping);
+
+  BodyNodePtr bn = pendulum->createJointAndBodyNodePair<BallJoint>(
+        nullptr, properties, BodyNode::Properties(name)).second;
+
+  // Make a shape for the Joint
+  const double& R = default_width;
+  std::shared_ptr<EllipsoidShape> ball(
+        new EllipsoidShape(sqrt(2)*Eigen::Vector3d(R, R, R)));
+  ball->setColor(dart::Color::Blue());
+  bn->addVisualizationShape(ball);
+
+  // Set the geometry of the Body
+  setGeometry(bn);
+
+  return bn;
+}
+
+BodyNode* addBody(const SkeletonPtr& pendulum,
+             BodyNode* parent, const std::string& name)
+{
+  // Set up the properties for the Joint
+  RevoluteJoint::Properties properties;
+  properties.mName = name+"_joint";
+  properties.mAxis = Eigen::Vector3d::UnitY();
+  properties.mT_ParentBodyToJoint.translation() =
+      Eigen::Vector3d(0, 0, default_height);
+  properties.mDampingCoefficient = default_damping;
+
+  // Create a new BodyNode, attached to its parent by a RevoluteJoint
+  BodyNodePtr bn = pendulum->createJointAndBodyNodePair<RevoluteJoint>(
+        parent, properties, BodyNode::Properties(name)).second;
+
+  // Make a shape for the Joint
+  const double R = default_width/2.0;
+  const double h = default_depth;
+  std::shared_ptr<CylinderShape> cyl(
+        new CylinderShape(R, h));
+  cyl->setColor(dart::Color::Blue());
+
+  // Line up the cylinder with the Joint axis
+  Eigen::Isometry3d tf(Eigen::Isometry3d::Identity());
+  tf.linear() = dart::math::eulerXYZToMatrix(
+        Eigen::Vector3d(90.0*M_PI/180.0, 0, 0));
+  cyl->setLocalTransform(tf);
+
+  bn->addVisualizationShape(cyl);
+
+  // Set the geometry of the Body
+  setGeometry(bn);
 
   return bn;
 }
@@ -224,7 +278,8 @@ int main(int argc, char* argv[])
   SkeletonPtr pendulum = Skeleton::create("pendulum");
 
   // Add each body to the last BodyNode in the pendulum
-  BodyNode* bn = addBody(pendulum, nullptr, "body1");
+//  BodyNode* bn = addBody(pendulum, nullptr, "body1");
+  BodyNode* bn = makeRootBody(pendulum, "body1");
   bn = addBody(pendulum, bn, "body2");
   bn = addBody(pendulum, bn, "body3");
   bn = addBody(pendulum, bn, "body4");
@@ -232,7 +287,7 @@ int main(int argc, char* argv[])
 
   // Set the initial position of the first DegreeOfFreedom so that the pendulum
   // starts to swing right away
-  pendulum->getDof(0)->setPosition(120*M_PI/180.0);
+  pendulum->getDof(1)->setPosition(120*M_PI/180.0);
 
   // Create a world and add the pendulum to the world
   WorldPtr world(new World);
