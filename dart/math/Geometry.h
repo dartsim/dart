d04c1ac1fd3fcd1a651e07c9d7c61ae2c8506918
M.X. Grey <mxgrey@gatech.edu>
2015-07-23 02:49:59 -0400

    added wrap to pi and mod convenience functions

--- dart/math/Geometry.h
+++ dart/math/Geometry.h
@@ -426,6 +426,22 @@ bool verifyRotation(const Eigen::Matrix3d& _R);
 /// all the elements are not NaN values.
 bool verifyTransform(const Eigen::Isometry3d& _T);
 
+/// Get the remainder of dividing x by y
+inline double mod(double x, double y)
+{
+  if( 0.0 == y )
+    return x;
+
+  return x - y * floor(x/y);
+}
+
+/// Compute the angle (in the range of -pi to +pi) which ignores any full
+/// rotations
+inline double wrapToPi(double angle)
+{
+  return mod(angle+M_PI, 2*M_PI) - M_PI;
+}
+
 template <typename MatrixType, typename ReturnType>
 void extractNullSpace(const Eigen::JacobiSVD<MatrixType>& _SVD, ReturnType& _NS)
 {
