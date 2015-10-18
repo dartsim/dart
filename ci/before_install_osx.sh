62b99ed422ec8590e3dbf394d3503c08306a02bd
Jeongseok Lee <jslee02@gmail.com>
2015-10-17 21:53:16 -0400

    Reduce log of travis ci on mac os

--- ci/before_install_osx.sh
+++ ci/before_install_osx.sh
@@ -1,23 +1,26 @@
 brew tap dartsim/dart
 brew tap homebrew/science
 
-brew update
+brew update > /dev/null
 
-brew install git
-# brew install cmake   # installed cmake-3.0.2
-brew install assimp
-brew install fcl
-brew install bullet
-brew install flann     # 1.8.14
-# brew install boost   # installed boost-1.55.2
-brew install eigen     # 3.2.2
-brew install tinyxml   # 2.6.2
-brew install tinyxml2  # 2.2.0
-brew install libccd    #
-brew install nlopt     # 2.4.2
-brew install ipopt
-brew install ros/deps/urdfdom
-brew install ros/deps/urdfdom_headers
-brew install ros/deps/console_bridge
-brew install ros/deps/gtest
+PACKAGES='
+git
+cmake
+assimp
+fcl
+bullet
+flann
+boost
+eigen
+tinyxml
+tinyxml2
+libccd
+nlopt
+ipopt
+ros/deps/urdfdom
+ros/deps/urdfdom_headers
+ros/deps/console_bridge
+ros/deps/gtest
+'
 
+brew install $PACKAGES | grep -v '%$'
