ab17051cc2ef4d48d9f8d0dbca99d0964ed33bd8
Jeongseok Lee <jslee02@gmail.com>
2016-01-08 07:31:24 -0500

    Use trusty (beta) for travis-ci build tests and enable osx

---
+++ ci/before_install_linux.sh
@@ -0,0 +1,33 @@
+sudo apt-add-repository --yes ppa:libccd-debs/ppa
+sudo apt-add-repository --yes ppa:fcl-debs/ppa
+sudo apt-add-repository --yes ppa:dartsim/ppa
+sudo apt-get -qq update
+
+APT_CORE='
+cmake
+libassimp-dev
+libboost-all-dev
+libccd-dev
+libeigen3-dev
+libfcl-dev
+'
+
+APT=$APT_CORE'
+freeglut3-dev
+libxi-dev
+libxmu-dev
+libflann-dev
+libnlopt-dev
+coinor-libipopt-dev
+libtinyxml-dev
+libtinyxml2-dev
+liburdfdom-dev
+liburdfdom-headers-dev
+'
+
+if [ $BUILD_CORE_ONLY = OFF ]; then
+  sudo apt-get -qq --yes --force-yes install $APT
+else
+  sudo apt-get -qq --yes --force-yes install $APT_CORE
+fi
+
