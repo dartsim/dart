a533e5d60d6d19618d1a0555112db1b7e1fb7db4
Jeongseok Lee <jslee02@gmail.com>
2016-01-11 19:01:19 -0500

    Replace dart_ with kido_

--- cmake/DARTMacros.cmake
+++ cmake/DARTMacros.cmake
@@ -1,9 +1,9 @@
 #===============================================================================
 # Appends items to a cached list.
 # Usage:
-#   dart_append_to_cached_string(_string _cacheDesc [items...])
+#   kido_append_to_cached_string(_string _cacheDesc [items...])
 #===============================================================================
-macro(dart_append_to_cached_string _string _cacheDesc)
+macro(kido_append_to_cached_string _string _cacheDesc)
   foreach(newItem ${ARGN})
     set(${_string} "${${_string}}${newItem}" CACHE INTERNAL ${_cacheDesc} FORCE)
   endforeach()
@@ -12,13 +12,13 @@ endmacro()
 #===============================================================================
 # Get list of file names give list of full paths.
 # Usage:
-#   dart_get_filename_components(_var _cacheDesc [items...])
+#   kido_get_filename_components(_var _cacheDesc [items...])
 #===============================================================================
-macro(dart_get_filename_components _var _cacheDesc)
+macro(kido_get_filename_components _var _cacheDesc)
   set(${_var} "" CACHE INTERNAL ${_cacheDesc} FORCE)
   foreach(header ${ARGN})
     get_filename_component(header ${header} NAME)
-    dart_append_to_cached_string(
+    kido_append_to_cached_string(
       ${_var}
       ${_cacheDesc}"_HEADER_NAMES"
       "${header}\;"
@@ -29,12 +29,12 @@ endmacro()
 #===============================================================================
 # Generate header file list to a cached list.
 # Usage:
-#   dart_generate_include_header_list(_var _target_dir _cacheDesc [headers...])
+#   kido_generate_include_header_list(_var _target_dir _cacheDesc [headers...])
 #===============================================================================
-macro(dart_generate_include_header_list _var _target_dir _cacheDesc)
+macro(kido_generate_include_header_list _var _target_dir _cacheDesc)
   set(${_var} "" CACHE INTERNAL ${_cacheDesc} FORCE)
   foreach(header ${ARGN})
-    dart_append_to_cached_string(
+    kido_append_to_cached_string(
       ${_var}
       ${_cacheDesc}"_HEADERS"
       "#include \"${_target_dir}${header}\"\n"
@@ -45,9 +45,9 @@ endmacro()
 #===============================================================================
 # Add library and set target properties
 # Usage:
-#   dart_add_library(_libname source1 [source2 ...])
+#   kido_add_library(_libname source1 [source2 ...])
 #===============================================================================
-macro(dart_add_library _name)
+macro(kido_add_library _name)
   add_library(${_name} ${ARGN})
   set_target_properties(
     ${_name} PROPERTIES
