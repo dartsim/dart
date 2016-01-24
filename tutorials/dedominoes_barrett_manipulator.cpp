2b23fed7b7f4afaca22a85f369aba990948cc24c
YangTian <tianyang9310@gmail.com>
2016-01-24 17:57:34 -0500

    add floor and joint limit for each joint

--- tutorials/dedominoes_barrett_manipulator.cpp
+++ tutorials/dedominoes_barrett_manipulator.cpp
@@ -30,6 +30,7 @@ protected:
 	//joint forces for the manipulator (output of the Controller)
 	Eigen::VectorXd mForces;
 };
+
 class MyWindow : public dart::gui::SimWindow
 {
 public:
@@ -48,7 +49,7 @@ public:
 			switch (key)
 			{
 				case ' ':
-					mHasEverRun = true;
+					mHasEverRun = !mHasEverRun;
 				break;
 			}
 		}
@@ -68,6 +69,32 @@ protected:
 	std::unique_ptr<Controller> mController;
 };
 
+SkeletonPtr createFloor()
+{
+	SkeletonPtr floor = Skeleton::create("floor");
+	
+	// Give the floor a body
+	BodyNodePtr body = floor->createJointAndBodyNodePair<WeldJoint>(nullptr).second;
+
+	// Give the body a shape
+	double floor_width = 5;
+	double floor_height = 0.25;
+
+	std::shared_ptr<BoxShape> box(
+			new BoxShape(Eigen::Vector3d(floor_width, floor_width, floor_height)));
+	box->setColor(dart::Color::Black());
+
+	body->addVisualizationShape(box);
+	body->addCollisionShape(box);
+
+	// put the body into position
+	Eigen::Isometry3d tf(Eigen::Isometry3d::Identity());
+	tf.translation() = Eigen::Vector3d(0.0, 0.0, -floor_height / 2.0);
+	body->getParentJoint()->setTransformFromParentBodyNode(tf);
+
+	return floor;
+}
+
 SkeletonPtr createManipulator()
 {
 	dart::utils::DartLoader loader;
@@ -75,22 +102,41 @@ SkeletonPtr createManipulator()
 		loader.parseSkeleton(DART_DATA_PATH"/my_urdf/customized_wam_7dof_wam_bhand.urdf");
 	manipulator->setName("manipulator");
 	Eigen::Isometry3d tf = Eigen::Isometry3d::Identity();
-	//tf.translation() = Eigen::Vector3d(0.000908563, -0.208275, -0.0256744);
-	
 
 	manipulator->getJoint(0)->setTransformFromParentBodyNode(tf);
+
+	// set initial position for each DOF
+	manipulator->getDof(1)->setPosition(-100.0 * M_PI / 180.0);
+	manipulator->getDof(3)->setPosition(180.0 * M_PI / 180.0);
+	
+	//set Joint position limit to make the manipulator not collapse into itself.
+	for (size_t i = 0 ; i < manipulator->getNumJoints(); ++ i)
+	{
+		manipulator->getJoint(i)->setPositionLimitEnforced(true);
+	}
+	
+	//manipulator->enableSelfCollision();
+
 	return manipulator;
 }
 
 
 int main(int argc, char*argv[])
 {
+	SkeletonPtr floor = createFloor();
 	SkeletonPtr manipulator = createManipulator();
 
 	WorldPtr world = std::make_shared<World>();
+	world->addSkeleton(floor);
 	world->addSkeleton(manipulator);
 
+
+
 	MyWindow window(world);
+
+	std::cout << "Before simulation has started, you can ..." <<std::endl;
+	std::cout << "The gravity is: [" << world->getGravity().transpose() << "]" << std::endl;
+	std::cout << "spacebar: initialize the simulation." <<std::endl;
 
 	glutInit(&argc, argv);
 	window.initWindow(640,480,"Wam Arm & Barrett Hand by Yang Tian");
