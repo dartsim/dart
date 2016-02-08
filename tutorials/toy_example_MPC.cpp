49655b2aa33d69fb7465151555a310482298d1c2
YangTian <tianyang9310@gmail.com>
2016-02-07 21:20:07 -0500

    inital commit of toy example for MPC

---
+++ tutorials/toy_example_MPC.cpp
@@ -0,0 +1,18 @@
+/*************************************************************************
+    > File Name: toy_example_MPC.cpp
+    > Author: Yang Tian
+    > Mail: tianyang9310@gmail.com 
+    > Created Time: Sun Feb  7 21:14:13 2016
+ ************************************************************************/
+#include "dart/dart.h"
+
+using namespace dart::dynamics;
+using namespace dart::simulation;
+using namespace dart::math;
+using namespace dart::gui;
+
+int main(int argc, char* argv[])
+{
+	return 0;
+
+}
