063e552a7f384a7925ecae5649225a8e3c140e42
Jeongseok Lee <jslee02@gmail.com>
2016-03-28 21:19:07 -0400

    Move files under osgDart to dart/gui/osg

---
+++ dart/gui/osg/render/BoxShapeNode.cpp
@@ -0,0 +1,210 @@
+/*
+ * Copyright (c) 2015, Georgia Tech Research Corporation
+ * All rights reserved.
+ *
+ * Author(s): Michael X. Grey <mxgrey@gatech.edu>
+ *
+ * Georgia Tech Graphics Lab and Humanoid Robotics Lab
+ *
+ * Directed by Prof. C. Karen Liu and Prof. Mike Stilman
+ * <karenliu@cc.gatech.edu> <mstilman@cc.gatech.edu>
+ *
+ * This file is provided under the following "BSD-style" License:
+ *   Redistribution and use in source and binary forms, with or
+ *   without modification, are permitted provided that the following
+ *   conditions are met:
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above
+ *     copyright notice, this list of conditions and the following
+ *     disclaimer in the documentation and/or other materials provided
+ *     with the distribution.
+ *   THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND
+ *   CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
+ *   INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ *   MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ *   DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
+ *   CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ *   SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ *   LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
+ *   USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+ *   AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ *   LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ *   ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ *   POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include <osg/Geode>
+#include <osg/ShapeDrawable>
+
+#include "dart/gui/osg/render/BoxShapeNode.h"
+#include "dart/gui/osg/Utils.h"
+
+#include "dart/dynamics/BoxShape.h"
+#include "dart/dynamics/SimpleFrame.h"
+
+namespace osgDart {
+namespace render {
+
+class BoxShapeGeode : public ShapeNode, public osg::Geode
+{
+public:
+
+  BoxShapeGeode(const std::shared_ptr<dart::dynamics::BoxShape>& shape,
+                ShapeFrameNode* parentShapeFrame);
+
+  void refresh();
+  void extractData();
+
+protected:
+
+  virtual ~BoxShapeGeode();
+
+  std::shared_ptr<dart::dynamics::BoxShape> mBoxShape;
+  BoxShapeDrawable* mDrawable;
+
+};
+
+//==============================================================================
+class BoxShapeDrawable : public osg::ShapeDrawable
+{
+public:
+
+  BoxShapeDrawable(dart::dynamics::BoxShape* shape,
+                   dart::dynamics::VisualAddon* visualAddon);
+
+  void refresh(bool firstTime);
+
+protected:
+
+  virtual ~BoxShapeDrawable();
+
+  dart::dynamics::BoxShape* mBoxShape;
+  dart::dynamics::VisualAddon* mVisualAddon;
+
+};
+
+//==============================================================================
+BoxShapeNode::BoxShapeNode(std::shared_ptr<dart::dynamics::BoxShape> shape,
+                           ShapeFrameNode* parent)
+  : ShapeNode(shape, parent, this),
+    mBoxShape(shape),
+    mGeode(nullptr)
+{
+  extractData(true);
+  setNodeMask(mVisualAddon->isHidden()? 0x0 : ~0x0);
+}
+
+//==============================================================================
+void BoxShapeNode::refresh()
+{
+  mUtilized = true;
+
+  setNodeMask(mVisualAddon->isHidden()? 0x0 : ~0x0);
+
+  if(mShape->getDataVariance() == dart::dynamics::Shape::STATIC)
+    return;
+
+  extractData(false);
+}
+
+//==============================================================================
+void BoxShapeNode::extractData(bool /*firstTime*/)
+{
+  if(nullptr == mGeode)
+  {
+    mGeode = new BoxShapeGeode(mBoxShape, mParentShapeFrameNode);
+    addChild(mGeode);
+    return;
+  }
+
+  mGeode->refresh();
+}
+
+//==============================================================================
+BoxShapeNode::~BoxShapeNode()
+{
+  // Do nothing
+}
+
+//==============================================================================
+BoxShapeGeode::BoxShapeGeode(
+    const std::shared_ptr<dart::dynamics::BoxShape>& shape,
+    ShapeFrameNode* parentShapeFrame)
+  : ShapeNode(shape, parentShapeFrame, this),
+    mBoxShape(shape),
+    mDrawable(nullptr)
+{
+  getOrCreateStateSet()->setMode(GL_BLEND, osg::StateAttribute::ON);
+  extractData();
+}
+
+//==============================================================================
+void BoxShapeGeode::refresh()
+{
+  mUtilized = true;
+
+  extractData();
+}
+
+//==============================================================================
+void BoxShapeGeode::extractData()
+{
+  if(nullptr == mDrawable)
+  {
+    mDrawable = new BoxShapeDrawable(mBoxShape.get(), mVisualAddon);
+    addDrawable(mDrawable);
+    return;
+  }
+
+  mDrawable->refresh(false);
+}
+
+//==============================================================================
+BoxShapeGeode::~BoxShapeGeode()
+{
+  // Do nothing
+}
+
+//==============================================================================
+BoxShapeDrawable::BoxShapeDrawable(dart::dynamics::BoxShape* shape,
+                                   dart::dynamics::VisualAddon* visualAddon)
+  : mBoxShape(shape),
+    mVisualAddon(visualAddon)
+{
+  refresh(true);
+}
+
+//==============================================================================
+void BoxShapeDrawable::refresh(bool firstTime)
+{
+  if(mBoxShape->getDataVariance() == dart::dynamics::Shape::STATIC)
+    setDataVariance(osg::Object::STATIC);
+  else
+    setDataVariance(osg::Object::DYNAMIC);
+
+  if(mBoxShape->checkDataVariance(dart::dynamics::Shape::DYNAMIC_PRIMITIVE)
+     || firstTime)
+  {
+    const Eigen::Vector3d& d = mBoxShape->getSize();
+    osg::ref_ptr<osg::Box> osg_shape = new osg::Box(osg::Vec3(0,0,0),
+                                                    d[0], d[1], d[2]);
+    setShape(osg_shape);
+    dirtyDisplayList();
+  }
+
+  if(mBoxShape->checkDataVariance(dart::dynamics::Shape::DYNAMIC_COLOR)
+     || firstTime)
+  {
+    setColor(eigToOsgVec4(mVisualAddon->getRGBA()));
+  }
+}
+
+//==============================================================================
+BoxShapeDrawable::~BoxShapeDrawable()
+{
+  // Do nothing
+}
+
+} // namespace render
+} // namespace osgDart
---
+++ dart/gui/osg/render/CylinderShapeNode.cpp
@@ -0,0 +1,220 @@
+/*
+ * Copyright (c) 2015, Georgia Tech Research Corporation
+ * All rights reserved.
+ *
+ * Author(s): Michael X. Grey <mxgrey@gatech.edu>
+ *
+ * Georgia Tech Graphics Lab and Humanoid Robotics Lab
+ *
+ * Directed by Prof. C. Karen Liu and Prof. Mike Stilman
+ * <karenliu@cc.gatech.edu> <mstilman@cc.gatech.edu>
+ *
+ * This file is provided under the following "BSD-style" License:
+ *   Redistribution and use in source and binary forms, with or
+ *   without modification, are permitted provided that the following
+ *   conditions are met:
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above
+ *     copyright notice, this list of conditions and the following
+ *     disclaimer in the documentation and/or other materials provided
+ *     with the distribution.
+ *   THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND
+ *   CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
+ *   INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ *   MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ *   DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
+ *   CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ *   SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ *   LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
+ *   USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+ *   AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ *   LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ *   ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ *   POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include <osg/Geode>
+#include <osg/ShapeDrawable>
+
+#include "dart/gui/osg/render/CylinderShapeNode.h"
+#include "dart/gui/osg/Utils.h"
+
+#include "dart/dynamics/CylinderShape.h"
+#include "dart/dynamics/SimpleFrame.h"
+
+namespace osgDart {
+namespace render {
+
+class CylinderShapeGeode : public ShapeNode, public osg::Geode
+{
+public:
+
+  CylinderShapeGeode(dart::dynamics::CylinderShape* shape,
+                     ShapeFrameNode* parent,
+                     CylinderShapeNode* parentNode);
+
+  void refresh();
+  void extractData();
+
+protected:
+
+  virtual ~CylinderShapeGeode();
+
+  dart::dynamics::CylinderShape* mCylinderShape;
+  CylinderShapeDrawable* mDrawable;
+
+};
+
+//==============================================================================
+class CylinderShapeDrawable : public osg::ShapeDrawable
+{
+public:
+
+  CylinderShapeDrawable(dart::dynamics::CylinderShape* shape,
+                        dart::dynamics::VisualAddon* visualAddon,
+                        CylinderShapeGeode* parent);
+
+  void refresh(bool firstTime);
+
+protected:
+
+  virtual ~CylinderShapeDrawable();
+
+  dart::dynamics::CylinderShape* mCylinderShape;
+  dart::dynamics::VisualAddon* mVisualAddon;
+
+  CylinderShapeGeode* mParent;
+
+};
+
+//==============================================================================
+CylinderShapeNode::CylinderShapeNode(
+    std::shared_ptr<dart::dynamics::CylinderShape> shape,
+    ShapeFrameNode* parent)
+  : ShapeNode(shape, parent, this),
+    mCylinderShape(shape),
+    mGeode(nullptr)
+{
+  extractData(true);
+  setNodeMask(mVisualAddon->isHidden()? 0x0 : ~0x0);
+}
+
+//==============================================================================
+void CylinderShapeNode::refresh()
+{
+  mUtilized = true;
+
+  setNodeMask(mVisualAddon->isHidden()? 0x0 : ~0x0);
+
+  if(mShape->getDataVariance() == dart::dynamics::Shape::STATIC)
+    return;
+
+  extractData(false);
+}
+
+//==============================================================================
+void CylinderShapeNode::extractData(bool /*firstTime*/)
+{
+  if(nullptr == mGeode)
+  {
+    mGeode = new CylinderShapeGeode(mCylinderShape.get(), mParentShapeFrameNode, this);
+    addChild(mGeode);
+    return;
+  }
+
+  mGeode->refresh();
+}
+
+//==============================================================================
+CylinderShapeNode::~CylinderShapeNode()
+{
+  // Do nothing
+}
+
+//==============================================================================
+CylinderShapeGeode::CylinderShapeGeode(
+    dart::dynamics::CylinderShape* shape,
+    ShapeFrameNode* parent,
+    CylinderShapeNode* parentNode)
+  : ShapeNode(parentNode->getShape(), parent, this),
+    mCylinderShape(shape),
+    mDrawable(nullptr)
+{
+  getOrCreateStateSet()->setMode(GL_BLEND, osg::StateAttribute::ON);
+  extractData();
+}
+
+//==============================================================================
+void CylinderShapeGeode::refresh()
+{
+  mUtilized = true;
+
+  extractData();
+}
+
+//==============================================================================
+void CylinderShapeGeode::extractData()
+{
+  if(nullptr == mDrawable)
+  {
+    mDrawable = new CylinderShapeDrawable(mCylinderShape, mVisualAddon, this);
+    addDrawable(mDrawable);
+    return;
+  }
+
+  mDrawable->refresh(false);
+}
+
+//==============================================================================
+CylinderShapeGeode::~CylinderShapeGeode()
+{
+  // Do nothing
+}
+
+//==============================================================================
+CylinderShapeDrawable::CylinderShapeDrawable(
+    dart::dynamics::CylinderShape* shape,
+    dart::dynamics::VisualAddon* visualAddon,
+    CylinderShapeGeode* parent)
+  : mCylinderShape(shape),
+    mVisualAddon(visualAddon),
+    mParent(parent)
+{
+  refresh(true);
+}
+
+//==============================================================================
+void CylinderShapeDrawable::refresh(bool firstTime)
+{
+  if(mCylinderShape->getDataVariance() == dart::dynamics::Shape::STATIC)
+    setDataVariance(osg::Object::STATIC);
+  else
+    setDataVariance(osg::Object::DYNAMIC);
+
+  if(mCylinderShape->checkDataVariance(dart::dynamics::Shape::DYNAMIC_PRIMITIVE)
+     || firstTime)
+  {
+    double R = mCylinderShape->getRadius();
+    double h = mCylinderShape->getHeight();
+    osg::ref_ptr<osg::Cylinder> osg_shape =
+        new osg::Cylinder(osg::Vec3(0,0,0), R, h);
+    setShape(osg_shape);
+    dirtyDisplayList();
+  }
+
+  if(mCylinderShape->checkDataVariance(dart::dynamics::Shape::DYNAMIC_COLOR)
+     || firstTime)
+  {
+    setColor(eigToOsgVec4(mVisualAddon->getRGBA()));
+  }
+}
+
+//==============================================================================
+CylinderShapeDrawable::~CylinderShapeDrawable()
+{
+  // Do nothing
+}
+
+} // namespace render
+} // namespace osgDart
