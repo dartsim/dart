e0828bf297d88b5fcad4534ca7d1c485f190f22e
Jeongseok Lee <jslee02@gmail.com>
2016-04-03 18:39:51 -0400

    Run coverage test after tests target is built

--- ci/script_linux.sh
+++ ci/script_linux.sh
@@ -1,7 +1,7 @@
 make
-if [ $COVERALLS = ON ]; then make coveralls; fi
 sudo ldconfig --verbose # So the test executeables can detect libtinyxml2
 if [ $BUILD_CORE_ONLY = OFF ]; then make all tutorials examples tests test; fi
+if [ $COVERALLS = ON ]; then make coveralls; fi
 # cd tools/
 # ./code_check.sh
 # ./abi_check.sh 4.1.0  # Check with DART 4.1.0
