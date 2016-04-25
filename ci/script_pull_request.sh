c8d6d7d00d66c36a46dea59edef12ca45248cc1e
Jeongseok Lee <jslee02@gmail.com>
2016-04-25 14:07:21 -0400

    Run full build tests only for pull request builds

---
+++ ci/script_pull_request.sh
@@ -0,0 +1,27 @@
+sudo apt-add-repository --yes ppa:libccd-debs/ppa
+sudo apt-add-repository --yes ppa:fcl-debs/ppa
+sudo apt-add-repository --yes ppa:dartsim/ppa
+sudo apt-get -qq update
+
+APT='
+cmake
+libassimp-dev
+libboost-all-dev
+libccd-dev
+libeigen3-dev
+libfcl-dev
+freeglut3-dev
+libxi-dev
+libxmu-dev
+libbullet-dev
+libflann-dev
+libnlopt-dev
+coinor-libipopt-dev
+libtinyxml-dev
+libtinyxml2-dev
+liburdfdom-dev
+liburdfdom-headers-dev
+libopenscenegraph-dev
+'
+
+sudo apt-get -qq --yes --force-yes install $APT
