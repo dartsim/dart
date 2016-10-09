2ba310834ed4fd42eb18c37b04197229ef45dee8
Jeongseok Lee <jslee02@gmail.com>
2016-10-09 03:31:13 -0400

    Set CMake option DART_VERBOSE ON for Travis-CI tests

--- ci/script.sh
+++ ci/script.sh
@@ -1,7 +1,7 @@
 if [ "$TRAVIS_PULL_REQUEST" = "false" ] && [ "$COMPILER" = "CLANG" ]; then exit; fi
 
 mkdir build && cd build
-cmake -DCMAKE_BUILD_TYPE=$BUILD_TYPE -DDART_TREAT_WARNINGS_AS_ERRORS=ON -DDART_COVERALLS=$COVERALLS ..
+cmake -DCMAKE_BUILD_TYPE=$BUILD_TYPE -DDART_VERBOSE=ON -DDART_TREAT_WARNINGS_AS_ERRORS=ON -DDART_COVERALLS=$COVERALLS ..
 make -j4 tutorials examples tests
 if [ $COVERALLS = ON ]; then make -j4 coveralls; else make -j4 test; fi
 
