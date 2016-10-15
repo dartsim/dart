20d3eee5745a7623c4d25e1c929782693dc34f76
Jeongseok Lee <jslee02@gmail.com>
2016-10-15 11:29:34 -0400

    Support urdf 1.0.0

--- dart/utils/urdf/urdf_world_parser.h
+++ dart/utils/urdf/urdf_world_parser.h
@@ -48,6 +48,8 @@
 #include <urdf_model/pose.h>
 #include <urdf_model/twist.h>
 
+#include "dart/utils/urdf/URDFTypes.hpp"
+
 namespace dart {
 namespace utils {
 namespace urdf_parsing {
@@ -63,7 +65,7 @@ public:
   /// Copy over a standard urdfEntity
   Entity(const urdf::Entity& urdfEntity);
 
-  boost::shared_ptr<urdf::ModelInterface> model;
+  urdf_shared_ptr<urdf::ModelInterface> model;
   urdf::Pose origin;
   urdf::Twist twist;
 
