67c9485b05198216097021077ee53bf7c8c1d548
Jeongseok Lee <jslee02@gmail.com>
2018-02-19 11:02:03 -0800

    Add installation test to Travis (#998)

--- ci/script.sh
+++ ci/script.sh
@@ -5,3 +5,12 @@ cmake -DCMAKE_BUILD_TYPE=$BUILD_TYPE -DDART_VERBOSE=ON -DDART_TREAT_WARNINGS_AS_
 if [ "$TRAVIS_OS_NAME" = "linux" ]; then make -j4 tutorials examples tests; else make -j4 tests; fi
 if [ "$TRAVIS_OS_NAME" = "linux" ]; then make check-format; fi
 if [ $CODECOV = ON ]; then make -j4 codecov; else make -j4 test; fi
+
+# Make sure we can install with no issues
+sudo make -j4 install
+
+# Build an example using installed DART
+cd $TRAVIS_BUILD_DIR/examples/rigidCubes
+mkdir build && cd build
+cmake ..
+make -j4
