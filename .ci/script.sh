e3db15539b3800b0b8560e1eb0dfabc2de54941b
Jeongseok Lee <jslee02@gmail.com>
2018-03-25 09:27:52 -0700

    Use ctest with --output-on-failure option

--- .ci/script.sh
+++ .ci/script.sh
@@ -6,7 +6,7 @@ if [ "$TRAVIS_PULL_REQUEST" = "false" ] && [ "$TRAVIS_OS_NAME" = "linux" ] && [ 
 
 mkdir build && cd build
 cmake -DCMAKE_BUILD_TYPE=$BUILD_TYPE -DDART_VERBOSE=ON -DDART_TREAT_WARNINGS_AS_ERRORS=ON -DDART_CODECOV=$CODECOV ..
-if [ "$TRAVIS_OS_NAME" = "linux" ]; then make -j4 tutorials examples tests; else make -j4 tests; fi
+if [ "$TRAVIS_OS_NAME" = "linux" ]; then make -j4 tutorials examples tests; else ctest --output-on-failure -j4; fi
 if [ "$TRAVIS_OS_NAME" = "linux" ]; then make check-format; fi
 if [ $CODECOV = ON ]; then make -j4 codecov; else make -j4 test; fi
 
