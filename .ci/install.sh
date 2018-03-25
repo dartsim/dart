f4a4fe84a21945bbcb74a7b6d763b7511bffc8d4
Jeongseok Lee <jslee02@gmail.com>
2018-03-25 07:56:05 -0700

    Rename ci to .ci

--- .ci/install.sh
+++ .ci/install.sh
@@ -4,5 +4,5 @@ set -e
 
 if [ "$TRAVIS_PULL_REQUEST" = "false" ] && [ "$TRAVIS_OS_NAME" = "linux" ] && [ "$COMPILER" = "CLANG" ]; then exit; fi
 
-if [ "$TRAVIS_OS_NAME" = "linux" ]; then 'ci/install_linux.sh' ; fi
-if [ "$TRAVIS_OS_NAME" = "osx"   ]; then 'ci/install_osx.sh'   ; fi
+if [ "$TRAVIS_OS_NAME" = "linux" ]; then '.ci/install_linux.sh' ; fi
+if [ "$TRAVIS_OS_NAME" = "osx"   ]; then '.ci/install_osx.sh'   ; fi
