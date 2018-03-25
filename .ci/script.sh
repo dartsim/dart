f3a20b924cb255a1f7e39ec4a6a950dd9f247f77
Jeongseok Lee <jslee02@gmail.com>
2018-03-25 09:59:57 -0700

    Fix building tests in script.sh

--- .ci/script.sh
+++ .ci/script.sh
@@ -5,9 +5,9 @@ if [ "$TRAVIS_PULL_REQUEST" = "false" ] && [ "$TRAVIS_OS_NAME" = "linux" ] && [ 
 
 mkdir build && cd build
 cmake -DCMAKE_BUILD_TYPE=$BUILD_TYPE -DDART_VERBOSE=ON -DDART_TREAT_WARNINGS_AS_ERRORS=ON -DDART_CODECOV=$CODECOV ..
-if [ "$TRAVIS_OS_NAME" = "linux" ]; then make -j4 tutorials examples tests; else ctest --output-on-failure -j4; fi
+if [ "$TRAVIS_OS_NAME" = "linux" ]; then make -j4 tutorials examples tests; else make -j4 tests; fi
 if [ "$TRAVIS_OS_NAME" = "linux" ]; then make check-format; fi
-if [ $CODECOV = ON ]; then make -j4 codecov; else make -j4 test; fi
+if [ $CODECOV = ON ]; then make -j4 codecov; else ctest --output-on-failure -j4; fi
 
 # Make sure we can install with no issues
 $SUDO make -j4 install
