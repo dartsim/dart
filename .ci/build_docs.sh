f0f73b544d0641bc6de96e0b14450ad2704f7a29
Jeongseok Lee <jslee02@gmail.com>
2018-08-08 23:35:54 -0700

    Better directory names

--- .ci/build_docs.sh
+++ .ci/build_docs.sh
@@ -2,50 +2,50 @@
 
 set -ex
 
-WORK_DIR="${HOME}/dart_docs"
-DART_DIR="${TRAVIS_BUILD_DIR}"
-
-mkdir -p ${WORK_DIR}
-cd ${WORK_DIR}
+DART_CLONE_DIR="${TRAVIS_BUILD_DIR}"
 
 # For branch builds, Travis only clones that branch with a fixed depth of 50
 # commits. This means that the clone knows nothing about other Git branches or
 # tags. We fix this by deleting and re-cloning the full repository.
-rm -rf "${DART_DIR}"
-git clone "https://github.com/${TRAVIS_REPO_SLUG}.git" ${DART_DIR}
+#rm -rf "${DART_CLONE_DIR}"
+cd "${DART_CLONE_DIR}"
+rm -rf *
+git clone "https://github.com/${TRAVIS_REPO_SLUG}.git" ${DART_CLONE_DIR}
 
 # TODO: For test. Should be removed before merging
-git -C ${DART_DIR} checkout auto_docs
+git -C ${DART_CLONE_DIR} checkout auto_docs
 
-# Organize into "${HOME}/dart_docs" directory
-DART_DOCS_DIR="${HOME}/dart_docs"
-mkdir -p ${DART_DOCS_DIR}
+WORK_DIR="${HOME}/dart_docs"
+mkdir -p ${WORK_DIR}
+cd ${WORK_DIR}
+
+DART_DOCS_BUILD_DIR="${WORK_DIR}/build/"
+mkdir -p ${DART_DOCS_BUILD_DIR}
+cd ${DART_DOCS_BUILD_DIR}
+
+# Organize into "${WORK_DIR}/docs" directory
+DART_DOCS_OUTPUT_DIR="${WORK_DIR}/docs/"
+mkdir -p ${DART_DOCS_OUTPUT_DIR}
 
 # Initialize list of API versions
-cat <<EOF > ${DART_DOCS_DIR}/README.md
+cat <<EOF > ${DART_DOCS_OUTPUT_DIR}/README.md
 # DART API Documentation
 EOF
 
-mkdir build_docs
-cd build_docs
-
 while read version; do
   if [[ ${version} == DART* ]]; then
-    echo "### ${version}" >> ${DART_DOCS_DIR}/README.md
+    echo "### ${version}" >> ${DART_DOCS_OUTPUT_DIR}/README.md
     continue
   fi
 
   # Add entry to list of API versions
-  echo "* [${version}](https://dartsim.github.io/dart/${version}/)" >> ${DART_DOCS_DIR}/README.md
+  echo "* [${version}](https://dartsim.github.io/dart/${version}/)" >> ${DART_DOCS_OUTPUT_DIR}/README.md
 
   # Build documentation
-  git -C ${DART_DIR} checkout ${version}
+  git -C ${DART_CLONE_DIR} checkout ${version}
   rm -rf *
-  cmake ${DART_DIR}
+  cmake ${DART_CLONE_DIR}
   make docs
 
-  # For debug; should be removed before merged
-  ls
-
-  mv doxygen/html ${DART_DOCS_DIR}/${version}
-done < ${DART_DIR}/.ci/docs_versions.txt
+  mv doxygen/html ${DART_DOCS_OUTPUT_DIR}/${version}
+done < ${DART_CLONE_DIR}/.ci/docs_versions.txt
