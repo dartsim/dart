5ae6510541552de975250648d0be7492ad71724a
Jeongseok Lee <jslee02@gmail.com>
2018-12-17 19:03:14 -0800

    Remove gtest source code
    * download using ExternalProject_Add()
    * upgrade gtest 1.8.1

---
+++ cmake/External_GTest.cmake
@@ -0,0 +1,58 @@
+find_package(Threads REQUIRED)
+
+include(ExternalProject)
+
+ExternalProject_Add(
+  googletest
+  GIT_REPOSITORY https://github.com/google/googletest.git
+  GIT_TAG release-1.8.1
+  UPDATE_COMMAND ""
+  INSTALL_COMMAND ""
+  LOG_DOWNLOAD ON
+  LOG_CONFIGURE ON
+  LOG_BUILD ON
+)
+
+ExternalProject_Get_Property(googletest source_dir)
+set(GTEST_INCLUDE_DIRS ${source_dir}/googletest/include)
+set(GMOCK_INCLUDE_DIRS ${source_dir}/googlemock/include)
+
+ExternalProject_Get_Property(googletest binary_dir)
+set(GTEST_LIBRARY_PATH ${binary_dir}/googlemock/gtest/${CMAKE_FIND_LIBRARY_PREFIXES}gtest.a)
+set(GTEST_LIBRARY gtest)
+add_library(${GTEST_LIBRARY} UNKNOWN IMPORTED)
+set_target_properties(${GTEST_LIBRARY} PROPERTIES
+  IMPORTED_LOCATION ${GTEST_LIBRARY_PATH}
+  IMPORTED_LINK_INTERFACE_LIBRARIES ${CMAKE_THREAD_LIBS_INIT}
+)
+add_dependencies(${GTEST_LIBRARY} googletest)
+
+set(GTEST_MAIN_LIBRARY_PATH ${binary_dir}/googlemock/gtest/${CMAKE_FIND_LIBRARY_PREFIXES}gtest_main.a)
+set(GTEST_MAIN_LIBRARY gtest_main)
+add_library(${GTEST_MAIN_LIBRARY} UNKNOWN IMPORTED)
+set_target_properties(${GTEST_MAIN_LIBRARY} PROPERTIES
+  IMPORTED_LOCATION ${GTEST_MAIN_LIBRARY_PATH}
+  IMPORTED_LINK_INTERFACE_LIBRARIES ${CMAKE_THREAD_LIBS_INIT}
+)
+target_link_libraries(${GTEST_MAIN_LIBRARY} INTERFACE ${GTEST_LIBRARY})
+add_dependencies(${GTEST_MAIN_LIBRARY} googletest)
+
+set(GMOCK_LIBRARY_PATH ${binary_dir}/googlemock/${CMAKE_FIND_LIBRARY_PREFIXES}gmock.a)
+set(GMOCK_LIBRARY gmock)
+add_library(${GMOCK_LIBRARY} UNKNOWN IMPORTED)
+set_target_properties(${GMOCK_LIBRARY} PROPERTIES
+  IMPORTED_LOCATION ${GMOCK_LIBRARY_PATH}
+  IMPORTED_LINK_INTERFACE_LIBRARIES ${CMAKE_THREAD_LIBS_INIT}
+)
+target_link_libraries(${GMOCK_LIBRARY} INTERFACE ${GTEST_LIBRARY})
+add_dependencies(${GMOCK_LIBRARY} googletest)
+
+set(GMOCK_MAIN_LIBRARY_PATH ${binary_dir}/googlemock/${CMAKE_FIND_LIBRARY_PREFIXES}gmock_main.a)
+set(GMOCK_MAIN_LIBRARY gmock_main)
+add_library(${GMOCK_MAIN_LIBRARY} UNKNOWN IMPORTED)
+set_target_properties(${GMOCK_MAIN_LIBRARY} PROPERTIES
+  IMPORTED_LOCATION ${GMOCK_MAIN_LIBRARY_PATH}
+  IMPORTED_LINK_INTERFACE_LIBRARIES ${CMAKE_THREAD_LIBS_INIT}
+)
+target_link_libraries(${GMOCK_MAIN_LIBRARY} INTERFACE ${GTEST_LIBRARY})
+add_dependencies(${GMOCK_MAIN_LIBRARY} ${GTEST_LIBRARY})
