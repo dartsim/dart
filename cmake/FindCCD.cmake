686c85aa10456ec4452925f36a8bfd24494cf23e
Jeongseok Lee <jslee02@gmail.com>
2019-01-06 23:23:21 -0800

    Use CCD target instead of variables

--- cmake/FindCCD.cmake
+++ cmake/FindCCD.cmake
@@ -14,49 +14,65 @@
 # CCD_LIBRARIES
 # CCD_VERSION
 
-find_package(PkgConfig QUIET)
+find_package(CCD QUIET CONFIG)
+# Upstream provide ccd-config.cmake since 2.1.
 
-# Check to see if pkgconfig is installed.
-pkg_check_modules(PC_CCD ccd QUIET)
+if(NOT CCD_FOUND)
 
-# Include directories
-# Give explicit precedence to ${PC_CCD_INCLUDEDIR}
-find_path(CCD_INCLUDE_DIRS
-    NAMES ccd/ccd.h
-    HINTS ${PC_CCD_INCLUDEDIR}
-    NO_DEFAULT_PATH
-    NO_CMAKE_PATH
-    NO_CMAKE_ENVIRONMENT_PATH
-    NO_SYSTEM_ENVIRONMENT_PATH)
-find_path(CCD_INCLUDE_DIRS
-    NAMES ccd/ccd.h
-    HINTS ${PC_CCD_INCLUDEDIR}
-    PATHS "${CMAKE_INSTALL_PREFIX}/include")
+  find_package(PkgConfig QUIET)
 
-# Libraries
-if(MSVC)
-  set(CCD_LIBRARIES optimized ccd debug ccdd)
-else()
-  # Give explicit precedence to ${PC_CCD_LIBDIR}
-  find_library(CCD_LIBRARIES
-      NAMES ccd
-      HINTS ${PC_CCD_LIBDIR}
+  # Check to see if pkgconfig is installed.
+  pkg_check_modules(PC_CCD ccd QUIET)
+
+  # Include directories
+  # Give explicit precedence to ${PC_CCD_INCLUDEDIR}
+  find_path(CCD_INCLUDE_DIRS
+      NAMES ccd/ccd.h
+      HINTS ${PC_CCD_INCLUDEDIR}
       NO_DEFAULT_PATH
       NO_CMAKE_PATH
       NO_CMAKE_ENVIRONMENT_PATH
       NO_SYSTEM_ENVIRONMENT_PATH)
-  find_library(CCD_LIBRARIES
-      NAMES ccd
-      HINTS ${PC_CCD_LIBDIR})
+  find_path(CCD_INCLUDE_DIRS
+      NAMES ccd/ccd.h
+      HINTS ${PC_CCD_INCLUDEDIR}
+      PATHS "${CMAKE_INSTALL_PREFIX}/include")
+
+  # Libraries
+  if(MSVC)
+    set(CCD_LIBRARIES optimized ccd debug ccdd)
+  else()
+    # Give explicit precedence to ${PC_CCD_LIBDIR}
+    find_library(CCD_LIBRARIES
+        NAMES ccd
+        HINTS ${PC_CCD_LIBDIR}
+        NO_DEFAULT_PATH
+        NO_CMAKE_PATH
+        NO_CMAKE_ENVIRONMENT_PATH
+        NO_SYSTEM_ENVIRONMENT_PATH)
+    find_library(CCD_LIBRARIES
+        NAMES ccd
+        HINTS ${PC_CCD_LIBDIR})
+  endif()
+
+  # Version
+  set(CCD_VERSION ${PC_CCD_VERSION})
+
+  # Set (NAME)_FOUND if all the variables and the version are satisfied.
+  include(FindPackageHandleStandardArgs)
+  find_package_handle_standard_args(CCD
+      FAIL_MESSAGE  DEFAULT_MSG
+      REQUIRED_VARS CCD_INCLUDE_DIRS CCD_LIBRARIES
+      VERSION_VAR   CCD_VERSION)
+
+  # Set target ccd if not set
+  # Upstream provides the target since 2.1
+  if(CCD_FOUND AND NOT TARGET ccd)
+  add_library(ccd INTERFACE IMPORTED)
+  set_target_properties(ccd PROPERTIES
+    INTERFACE_INCLUDE_DIRECTORIES "${CCD_INCLUDE_DIRS}"
+    INTERFACE_LINK_LIBRARIES "${CCD_LIBRARIES}"
+  )
+  endif()
+
 endif()
-
-# Version
-set(CCD_VERSION ${PC_CCD_VERSION})
-
-# Set (NAME)_FOUND if all the variables and the version are satisfied.
-include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(CCD
-    FAIL_MESSAGE  DEFAULT_MSG
-    REQUIRED_VARS CCD_INCLUDE_DIRS CCD_LIBRARIES
-    VERSION_VAR   CCD_VERSION)
-
