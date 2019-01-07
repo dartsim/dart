04b5f8d3da2c4394509677cf060c6a39b3ef70ba
Jeongseok Lee <jslee02@gmail.com>
2019-01-06 20:39:20 -0800

    Update finding Eigen3 logic

--- cmake/DARTFindEigen3.cmake
+++ cmake/DARTFindEigen3.cmake
@@ -6,9 +6,18 @@
 #
 # This file is provided under the "BSD-style" License
 
-find_package(Eigen3 3.0.5 REQUIRED)
+# We intentionally don't specify the required Eigen3 version like
+# find_package(Eigen3 3.2.92) because the version file is not provided by
+# upstream until 3.3.1.
+find_package(Eigen3 REQUIRED)
+if(EIGEN3_VERSION VERSION_LESS 3.2.92)  # requires Eigen3 3.3~beta1
+  message(FATAL_ERROR
+    "Eigen3 ${EIGEN3_VERSION} is found but >= 3.2.92 (3.3~beta1) is required"
+  )
+endif()
 
 # Set target Eigen3::Eigen if not set
+# The target is not imported by upstream until 3.3.2
 if(EIGEN3_FOUND AND NOT TARGET Eigen3::Eigen)
   add_library(Eigen3::Eigen INTERFACE IMPORTED)
   set_target_properties(Eigen3::Eigen PROPERTIES
