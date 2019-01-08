22d23189073c0a44be200bd5407f7434050322c8
Jeongseok Lee <jslee02@gmail.com>
2019-01-07 19:47:16 -0800

    Manual version check for Assimp

--- cmake/DARTFindassimp.cmake
+++ cmake/DARTFindassimp.cmake
@@ -6,7 +6,16 @@
 #
 # This file is provided under the "BSD-style" License
 
-find_package(assimp 3.2 REQUIRED)
+find_package(assimp REQUIRED)
+
+# Manually check version because the upstream version compatibility policy
+# doesn't allow different major number while DART is compatible any version
+# greater than or equal to 3.2.
+if(ASSIMP_VERSION VERSION_LESS 3.2)
+    message(STATUS "Found Assimp ${ASSIMP_VERSION}, but Assimp >= 3.2 is
+      required"
+    )
+endif()
 
 # Set target assimp if not set
 # The target is not imported by upstream until 4.1
