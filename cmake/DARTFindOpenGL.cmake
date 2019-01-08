072584364a0df676722346208a7cbbc3050556f5
Jeongseok Lee <jslee02@gmail.com>
2019-01-08 02:28:24 -0800

    Define OpenGL targets if not defined

--- cmake/DARTFindOpenGL.cmake
+++ cmake/DARTFindOpenGL.cmake
@@ -7,3 +7,19 @@
 # This file is provided under the "BSD-style" License
 
 find_package(OpenGL QUIET)
+
+if(OPENGL_FOUND AND NOT TARGET OpenGL::GL)
+  add_library(OpenGL::GL INTERFACE IMPORTED)
+  set_target_properties(OpenGL::GL PROPERTIES
+    INTERFACE_INCLUDE_DIRECTORIES "${OPENGL_INCLUDE_DIR}"
+    INTERFACE_LINK_LIBRARIES "${OPENGL_gl_LIBRARY}"
+  )
+endif()
+
+if(OPENGL_FOUND AND NOT TARGET OpenGL::GLU)
+  add_library(OpenGL::GLU INTERFACE IMPORTED)
+  set_target_properties(OpenGL::GLU PROPERTIES
+    INTERFACE_INCLUDE_DIRECTORIES "${OPENGL_INCLUDE_DIR}"
+    INTERFACE_LINK_LIBRARIES "${OPENGL_glu_LIBRARY}"
+  )
+endif()
