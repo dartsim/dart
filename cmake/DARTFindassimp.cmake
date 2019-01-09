d7bebf325448f1bc8ca088f52733b3b52c2722ee
Jeongseok Lee <jslee02@gmail.com>
2019-01-09 13:22:43 -0800

    Use own module to find assimp since one from upstream isn't compatible with macOS (at least Mojave)

--- cmake/DARTFindassimp.cmake
+++ cmake/DARTFindassimp.cmake
@@ -8,23 +8,11 @@
 
 find_package(assimp REQUIRED)
 
-set(ASSIMP_VERSION ${assimp_VERSION})
-
 # Manually check version because the upstream version compatibility policy
 # doesn't allow different major number while DART is compatible any version
 # greater than or equal to 3.2.
 if(ASSIMP_VERSION VERSION_LESS 3.2)
   message(STATUS "Found Assimp ${ASSIMP_VERSION}, but Assimp >= 3.2 is
     required"
-  )
-endif()
-
-# Set target assimp if not set
-# The target is not imported by upstream until 4.1
-if(assimp_FOUND AND NOT TARGET assimp)
-  add_library(assimp INTERFACE IMPORTED)
-  set_target_properties(assimp PROPERTIES
-    INTERFACE_INCLUDE_DIRECTORIES "${ASSIMP_INCLUDE_DIRS}"
-    INTERFACE_LINK_LIBRARIES "${ASSIMP_LIBRARIES}"
   )
 endif()
---
+++ cmake/Findassimp.cmake
@@ -0,0 +1,53 @@
+# Copyright (c) 2011-2018, The DART development contributors
+# All rights reserved.
+#
+# The list of contributors can be found at:
+#   https://github.com/dartsim/dart/blob/master/LICENSE
+#
+# This file is provided under the "BSD-style" License
+
+# Find ASSIMP
+#
+# This sets the following variables:
+# ASSIMP_FOUND
+# ASSIMP_INCLUDE_DIRS
+# ASSIMP_LIBRARIES
+# ASSIMP_VERSION
+
+find_package(PkgConfig QUIET)
+
+# Check to see if pkgconfig is installed.
+pkg_check_modules(PC_ASSIMP assimp QUIET)
+
+# Include directories
+find_path(ASSIMP_INCLUDE_DIRS assimp/scene.h
+    HINTS ${PC_ASSIMP_INCLUDEDIR}
+    PATHS "${CMAKE_INSTALL_PREFIX}/include")
+
+# Libraries
+if(MSVC)
+  set(ASSIMP_LIBRARIES optimized assimp debug assimpd)
+else()
+  find_library(ASSIMP_LIBRARIES
+      NAMES assimp
+      HINTS ${PC_ASSIMP_LIBDIR})
+endif()
+
+# Version
+set(ASSIMP_VERSION ${PC_ASSIMP_VERSION})
+
+# Set (NAME)_FOUND if all the variables and the version are satisfied.
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(ASSIMP
+    FAIL_MESSAGE  DEFAULT_MSG
+    REQUIRED_VARS ASSIMP_INCLUDE_DIRS ASSIMP_LIBRARIES
+    VERSION_VAR   ASSIMP_VERSION)
+
+# Set target assimp if not set
+if(ASSIMP_FOUND AND NOT TARGET assimp)
+  add_library(assimp INTERFACE IMPORTED)
+  set_target_properties(assimp PROPERTIES
+    INTERFACE_INCLUDE_DIRECTORIES "${ASSIMP_INCLUDE_DIRS}"
+    INTERFACE_LINK_LIBRARIES "${ASSIMP_LIBRARIES}"
+  )
+endif()
