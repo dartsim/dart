0639ad4340833d3d3dd68cacc69f3558709bb9c7
Jeongseok Lee <jslee02@gmail.com>
2019-02-12 00:35:57 -0800

    Install more dependencies

--- .ci/install_linux.sh
+++ .ci/install_linux.sh
@@ -61,7 +61,7 @@ fi
 if [ "$BUILD_NAME" = "DARTPY" ]; then
   sudo apt-add-repository -y ppa:personalrobotics
   sudo apt-get -qq update
-  sudo apt-get -y install libboost-python-dev
+  sudo apt-get -y install libboost-dev libboost-thread-dev libboost-python-dev
   sudo apt-get -y install python3-dev python3-numpy python3-boost-numpy-eigen
 fi
 
