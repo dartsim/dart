578327ea25885ffa6a87e5be31ec5e86964ef0a7
Jeongseok Lee <jslee02@gmail.com>
2019-03-03 19:56:09 -0800

    Revert change of Findassimp.cmake

--- cmake/Findassimp.cmake
+++ cmake/Findassimp.cmake
@@ -14,37 +14,31 @@
 # ASSIMP_LIBRARIES
 # ASSIMP_VERSION
 
-find_package(assimp QUIET CONFIG)
+find_package(PkgConfig QUIET)
 
-if(NOT ASSIMP_FOUND AND NOT assimp_FOUND)
+# Check to see if pkgconfig is installed.
+pkg_check_modules(PC_ASSIMP assimp QUIET)
 
-  find_package(PkgConfig QUIET)
+# Include directories
+find_path(ASSIMP_INCLUDE_DIRS assimp/scene.h
+    HINTS ${PC_ASSIMP_INCLUDEDIR}
+    PATHS "${CMAKE_INSTALL_PREFIX}/include")
 
-  # Check to see if pkgconfig is installed.
-  pkg_check_modules(PC_ASSIMP assimp QUIET)
-
-  # Include directories
-  find_path(ASSIMP_INCLUDE_DIRS assimp/scene.h
-      HINTS ${PC_ASSIMP_INCLUDEDIR}
-      PATHS "${CMAKE_INSTALL_PREFIX}/include")
-
-  # Libraries
-  if(MSVC)
-    set(ASSIMP_LIBRARIES optimized assimp debug assimpd)
-  else()
-    find_library(ASSIMP_LIBRARIES
-        NAMES assimp
-        HINTS ${PC_ASSIMP_LIBDIR})
-  endif()
-
-  # Version
-  set(ASSIMP_VERSION ${PC_ASSIMP_VERSION})
-
-  # Set (NAME)_FOUND if all the variables and the version are satisfied.
-  include(FindPackageHandleStandardArgs)
-  find_package_handle_standard_args(assimp
-      FAIL_MESSAGE  DEFAULT_MSG
-      REQUIRED_VARS ASSIMP_INCLUDE_DIRS ASSIMP_LIBRARIES
-      VERSION_VAR   ASSIMP_VERSION)
-
+# Libraries
+if(MSVC)
+  set(ASSIMP_LIBRARIES optimized assimp debug assimpd)
+else()
+  find_library(ASSIMP_LIBRARIES
+      NAMES assimp
+      HINTS ${PC_ASSIMP_LIBDIR})
 endif()
+
+# Version
+set(ASSIMP_VERSION ${PC_ASSIMP_VERSION})
+
+# Set (NAME)_FOUND if all the variables and the version are satisfied.
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(assimp
+    FAIL_MESSAGE  DEFAULT_MSG
+    REQUIRED_VARS ASSIMP_INCLUDE_DIRS ASSIMP_LIBRARIES
+    VERSION_VAR   ASSIMP_VERSION)
