4a784fc44a2ff3f7d4a6fa115f8efdc60147d1c2
Jeongseok Lee <jslee02@gmail.com>
2019-03-03 19:20:57 -0800

    Try first to find assimp using module

--- cmake/Findassimp.cmake
+++ cmake/Findassimp.cmake
@@ -14,31 +14,37 @@
 # ASSIMP_LIBRARIES
 # ASSIMP_VERSION
 
-find_package(PkgConfig QUIET)
+find_package(assimp QUIET CONFIG)
 
-# Check to see if pkgconfig is installed.
-pkg_check_modules(PC_ASSIMP assimp QUIET)
+if(NOT ASSIMP_FOUND AND NOT assimp_FOUND)
 
-# Include directories
-find_path(ASSIMP_INCLUDE_DIRS assimp/scene.h
-    HINTS ${PC_ASSIMP_INCLUDEDIR}
-    PATHS "${CMAKE_INSTALL_PREFIX}/include")
+  find_package(PkgConfig QUIET)
 
-# Libraries
-if(MSVC)
-  set(ASSIMP_LIBRARIES optimized assimp debug assimpd)
-else()
-  find_library(ASSIMP_LIBRARIES
-      NAMES assimp
-      HINTS ${PC_ASSIMP_LIBDIR})
+  # Check to see if pkgconfig is installed.
+  pkg_check_modules(PC_ASSIMP assimp QUIET)
+
+  # Include directories
+  find_path(ASSIMP_INCLUDE_DIRS assimp/scene.h
+      HINTS ${PC_ASSIMP_INCLUDEDIR}
+      PATHS "${CMAKE_INSTALL_PREFIX}/include")
+
+  # Libraries
+  if(MSVC)
+    set(ASSIMP_LIBRARIES optimized assimp debug assimpd)
+  else()
+    find_library(ASSIMP_LIBRARIES
+        NAMES assimp
+        HINTS ${PC_ASSIMP_LIBDIR})
+  endif()
+
+  # Version
+  set(ASSIMP_VERSION ${PC_ASSIMP_VERSION})
+
+  # Set (NAME)_FOUND if all the variables and the version are satisfied.
+  include(FindPackageHandleStandardArgs)
+  find_package_handle_standard_args(assimp
+      FAIL_MESSAGE  DEFAULT_MSG
+      REQUIRED_VARS ASSIMP_INCLUDE_DIRS ASSIMP_LIBRARIES
+      VERSION_VAR   ASSIMP_VERSION)
+
 endif()
-
-# Version
-set(ASSIMP_VERSION ${PC_ASSIMP_VERSION})
-
-# Set (NAME)_FOUND if all the variables and the version are satisfied.
-include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(assimp
-    FAIL_MESSAGE  DEFAULT_MSG
-    REQUIRED_VARS ASSIMP_INCLUDE_DIRS ASSIMP_LIBRARIES
-    VERSION_VAR   ASSIMP_VERSION)
