f54ee2dbec71518e76d14c6018ee7996d3bd4128
Steven Peters <scpeters@openrobotics.org>
2019-07-13 11:45:38 -0700

    Constraints: allow constraint force mixing > 1 (#1371)
    * Constraints: allow cfm > 1
    
    * Update CHANGELOG.md

--- dart/constraint/JointLimitConstraint.cpp
+++ dart/constraint/JointLimitConstraint.cpp
@@ -162,12 +162,6 @@ void JointLimitConstraint::setConstraintForceMixing(double _cfm)
            << "] is lower than 1e-9. " << "It is set to 1e-9." << std::endl;
     mConstraintForceMixing = 1e-9;
   }
-  if (_cfm > 1.0)
-  {
-    dtwarn << "Constraint force mixing parameter[" << _cfm
-           << "] is greater than 1.0. " << "It is set to 1.0." << std::endl;
-    mConstraintForceMixing = 1.0;
-  }
 
   mConstraintForceMixing = _cfm;
 }
