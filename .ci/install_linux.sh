afed2a2a65ba42c4256ac4b5570f009144928762
Jeongseok Lee <jeoslee@amazon.com>
2020-08-28 11:28:41 -0700

    Build pagmo2 without nlopt and ipopt support

--- .ci/install_linux.sh
+++ .ci/install_linux.sh
@@ -90,9 +90,10 @@ git clone https://github.com/esa/pagmo2.git -b 'v2.15.0' --single-branch --depth
   cd pagmo2 && mkdir build && cd build &&
   cmake .. \
     -DCMAKE_BUILD_TYPE=Release \
-    -DPAGMO_WITH_EIGEN3=ON \
-    -DPAGMO_WITH_NLOPT=ON \
-    -DPAGMO_WITH_IPOPT=ON &&
+    -DPAGMO_WITH_EIGEN3=ON &&
+  # NLopt and IPopt supports are disabled until https://github.com/esa/pagmo2/issues/445 resolved
+  # -DPAGMO_WITH_NLOPT=ON \
+  # -DPAGMO_WITH_IPOPT=ON \
   make -j$(nproc) &&
   make install
 
