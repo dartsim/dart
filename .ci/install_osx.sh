6042bb61d639941b0d3110ed73a8c8032db8d3ac
Jeongseok Lee <jeoslee@amazon.com>
2020-08-28 13:52:38 -0700

    Install tbb

--- .ci/install_osx.sh
+++ .ci/install_osx.sh
@@ -6,6 +6,7 @@ brew bundle || brew bundle
 brew install open-scene-graph --HEAD  # install master branch until 3.7.0 is released
 
 # pagmo2: build from source until https://github.com/esa/pagmo2/issues/445 is resolved
+brew install tbb
 git clone https://github.com/esa/pagmo2.git -b 'v2.15.0' --single-branch --depth 1 &&
   cd pagmo2 && mkdir build && cd build &&
   cmake .. \
