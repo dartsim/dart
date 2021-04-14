234db423e53baba351a0844071aedc7ffc852db4
Jeongseok Lee <jeoslee@amazon.com>
2021-04-14 02:04:56 -0700

    Move code coverage build from Travis to GitHub Actions

--- .ci/build.sh
+++ .ci/build.sh
@@ -147,7 +147,15 @@ fi
 
 # Codecov
 if [ "$CODECOV" = "ON" ]; then
-  make -j$num_threads codecov
+  lcov --directory . --capture --output-file coverage.info
+  # filter out system and extra files.
+  # To also not include test code in coverage add them with full path to the patterns: '*/tests/*'
+  lcov --remove coverage.info '/usr/*' "${HOME}"'/.cache/*' --output-file coverage.info
+  # output coverage data for debugging (optional)
+  lcov --list coverage.info
+  # Uploading to CodeCov
+  # '-f' specifies file(s) to use and disables manual coverage gathering and file search which has already been done above
+  bash <(curl -s https://codecov.io/bash) -f coverage.info || echo "Codecov did not collect coverage reports"
 fi
 
 # DART: build an C++ example using installed DART
