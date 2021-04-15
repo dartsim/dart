8f2ec7995fb444b7112af1b5dc00203b81b0c63e
Jeongseok Lee <jeoslee@amazon.com>
2021-04-15 10:14:52 -0700

    Drop supporting Eigen < 3.3.4

--- cmake/DARTFindEigen3.cmake
+++ cmake/DARTFindEigen3.cmake
@@ -6,21 +6,4 @@
 #
 # This file is provided under the "BSD-style" License
 
-# We intentionally don't specify the required Eigen3 version like
-# find_package(Eigen3 3.2.92) because the version file is not provided by
-# upstream until 3.3.1.
-find_package(Eigen3 REQUIRED CONFIG)
-if(EIGEN3_VERSION_STRING VERSION_LESS 3.2.92)  # 3.3~beta1
-  message(FATAL_ERROR "Eigen3 ${EIGEN3_VERSION_STRING} is found but >= 3.2.92 "
-    "(3.3~beta1) is required"
-  )
-endif()
-
-# Set target Eigen3::Eigen if not set
-# The target is not imported by upstream until 3.3.2
-if(Eigen3_FOUND AND NOT TARGET Eigen3::Eigen)
-  add_library(Eigen3::Eigen INTERFACE IMPORTED)
-  set_target_properties(Eigen3::Eigen PROPERTIES
-    INTERFACE_INCLUDE_DIRECTORIES "${EIGEN3_INCLUDE_DIRS}"
-  )
-endif()
+find_package(Eigen3 3.3.4 REQUIRED CONFIG)
--- dart/common/detail/Memory-impl.hpp
+++ dart/common/detail/Memory-impl.hpp
@@ -34,14 +34,11 @@
 #define DART_COMMON_DETAIL_MEMORY_IMPL_HPP_
 
 #include <memory>
-#include <Eigen/Core>
-#include "dart/config.hpp"
 
-#if EIGEN_VERSION_AT_LEAST(3, 2, 1) && EIGEN_VERSION_AT_MOST(3, 2, 8)
-#  include "dart/common/detail/AlignedAllocator.hpp"
-#else
-#  include <Eigen/StdVector>
-#endif
+#include <Eigen/Core>
+#include <Eigen/StdVector>
+
+#include "dart/config.hpp"
 
 namespace dart {
 namespace common {
@@ -52,14 +49,8 @@ std::shared_ptr<_Tp> make_aligned_shared(_Args&&... __args)
 {
   using _Tp_nc = typename std::remove_const<_Tp>::type;
 
-#if EIGEN_VERSION_AT_LEAST(3, 2, 1) && EIGEN_VERSION_AT_MOST(3, 2, 8)
-  return std::allocate_shared<_Tp>(
-      detail::aligned_allocator_cpp11<_Tp_nc>(),
-      std::forward<_Args>(__args)...);
-#else
   return std::allocate_shared<_Tp>(
       Eigen::aligned_allocator<_Tp_nc>(), std::forward<_Args>(__args)...);
-#endif // EIGEN_VERSION_AT_LEAST(3,2,1) && EIGEN_VERSION_AT_MOST(3,2,8)
 }
 
 //==============================================================================
