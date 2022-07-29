7cc82690d2e1f503cc94167df9482aca5a633dd5
Julian Meyers <54123965+J-Meyers@users.noreply.github.com>
2022-07-29 15:28:43 -0500

    Silence Clang-14 bitwise instead of logical (#1674)
    * Silence Clang-14 bitwise instead of logical
    
    * Switch to logical and 1
    
    Co-authored-by: Jeongseok (JS) Lee <jslee02@users.noreply.github.com>
    
    * Switch to logical and 2
    
    Co-authored-by: Jeongseok (JS) Lee <jslee02@users.noreply.github.com>
    
    Co-authored-by: Jeongseok (JS) Lee <jslee02@users.noreply.github.com>

--- dart/utils/mjcf/MjcfParser.cpp
+++ dart/utils/mjcf/MjcfParser.cpp
@@ -306,7 +306,7 @@ createJointAndBodyNodePairForMultipleJoints(
 
       // Limits
       props.mIsPositionLimitEnforced
-          = mjcfJoint0.isLimited() & mjcfJoint1.isLimited();
+          = mjcfJoint0.isLimited() && mjcfJoint1.isLimited();
       props.mPositionLowerLimits[0] = mjcfJoint0.getRange()[0];
       props.mPositionLowerLimits[1] = mjcfJoint1.getRange()[0];
       props.mPositionUpperLimits[0] = mjcfJoint0.getRange()[1];
@@ -365,7 +365,7 @@ createJointAndBodyNodePairForMultipleJoints(
 
       // Limits
       props.mIsPositionLimitEnforced
-          = mjcfJoint0.isLimited() & mjcfJoint1.isLimited();
+          = mjcfJoint0.isLimited() && mjcfJoint1.isLimited();
       props.mPositionLowerLimits[0] = mjcfJoint0.getRange()[0];
       props.mPositionLowerLimits[1] = mjcfJoint1.getRange()[0];
       props.mPositionLowerLimits[2] = mjcfJoint2.getRange()[0];
