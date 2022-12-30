0827d49e9823267a1febebda9707e93553b26173
Jeongseok Lee <jslee02@gmail.com>
2022-12-29 21:59:49 -0800

    Fix header list macro

--- cmake/DARTMacros.cmake
+++ cmake/DARTMacros.cmake
@@ -5,7 +5,7 @@
 #===============================================================================
 macro(dart_append_to_cached_string _string _cacheDesc)
   foreach(newItem ${ARGN})
-    set(${_string} "${${_string}}${newItem}" CACHE INTERNAL ${_cacheDesc} FORCE)
+    set(${_string} "${${_string}}${newItem}")
   endforeach()
 endmacro()
 
@@ -15,7 +15,7 @@ endmacro()
 #   dart_get_filename_components(_var _cacheDesc [items...])
 #===============================================================================
 macro(dart_get_filename_components _var _cacheDesc)
-  set(${_var} "" CACHE INTERNAL ${_cacheDesc} FORCE)
+  set(${_var} "")
   foreach(header ${ARGN})
     get_filename_component(header ${header} NAME)
     dart_append_to_cached_string(
@@ -40,24 +40,6 @@ macro(dart_get_subdir_list var curdir)
         endif()
     endforeach()
     set(${var} ${dirlist})
-endmacro()
-
-#===============================================================================
-# DEPRECATED in 6.7 (see #1081)
-#
-# Generate header file list to a cached list.
-# Usage:
-#   dart_generate_include_header_list(_var _target_dir _cacheDesc [headers...])
-#===============================================================================
-macro(dart_generate_include_header_list _var _target_dir _cacheDesc)
-  set(${_var} "" CACHE INTERNAL ${_cacheDesc} FORCE)
-  foreach(header ${ARGN})
-    dart_append_to_cached_string(
-      ${_var}
-      ${_cacheDesc}"_HEADERS"
-      "#include \"${_target_dir}${header}\"\n"
-    )
-  endforeach()
 endmacro()
 
 #===============================================================================
