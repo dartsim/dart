abc4775b9dccf90626fe9ed0b222193aed83bc6a
Jeongseok Lee <jslee02@gmail.com>
2023-01-20 18:13:32 -0800

    Add dart_option() macro

--- cmake/DARTMacros.cmake
+++ cmake/DARTMacros.cmake
@@ -1,3 +1,112 @@
+# Copyright (c) 2011-2023, The DART development contributors
+# All rights reserved.
+#
+# The list of contributors can be found at:
+#   https://github.com/dartsim/dart/blob/master/LICENSE
+#
+# This file is provided under the "BSD-style" License
+
+# ==============================================================================
+# dart_get_max(var [value1 value2...])
+function(dart_get_max var)
+  set(first YES)
+  set(choice NO)
+  foreach(item ${ARGN})
+    if(first)
+      set(choice ${item})
+      set(first NO)
+    elseif(choice LESS ${item})
+      set(choice ${item})
+    endif()
+  endforeach(item)
+  set(${var} ${choice} PARENT_SCOPE)
+endfunction()
+
+# ==============================================================================
+# dart_get_max_string_length(var [value1 value2...])
+function(dart_get_max_string_length var)
+  foreach(item ${ARGN})
+    string(LENGTH ${item} length)
+    list(APPEND list ${length})
+  endforeach()
+  dart_get_max(choice ${list})
+  set(${var} ${choice} PARENT_SCOPE)
+endfunction()
+
+# ==============================================================================
+# cmake-format: off
+# dart_option(<variable> "<help_text>" <value>)
+# cmake-format: on
+function(dart_option variable help_text default_value)
+  set_property(
+    GLOBAL PROPERTY DART_DETAIL_PROPERTY_OPTION_VARIABLE "${variable}" APPEND
+  )
+  set_property(
+    GLOBAL PROPERTY DART_DETAIL_property_option_help_text "${help_text}" APPEND
+  )
+  set_property(
+    GLOBAL PROPERTY DART_DETAIL_property_option_default_value "${default_value}"
+    APPEND
+  )
+
+  # Add option
+  option(${variable} ${help_text} ${default_value})
+
+  # Normalize boolean value variants (e.g. 1/0, On/Off, TRUE/FALSE) to ON/OFF
+  if(${variable})
+    set(${variable} ON PARENT_SCOPE)
+  else()
+    set(${variable} OFF PARENT_SCOPE)
+  endif()
+
+endfunction()
+
+# ==============================================================================
+# cmake-format: off
+# dart_print_options()
+# cmake-format: on
+function(dart_print_options)
+  # Print the header
+  message(STATUS "[ Options ]")
+
+  get_property(
+    option_variables GLOBAL PROPERTY DART_DETAIL_PROPERTY_OPTION_VARIABLE
+  )
+  get_property(
+    option_help_texts GLOBAL PROPERTY DART_DETAIL_property_option_help_text
+  )
+  get_property(
+    option_default_values GLOBAL
+    PROPERTY DART_DETAIL_property_option_default_value
+  )
+
+  dart_get_max_string_length(option_variable_max_len ${option_variables})
+  list(LENGTH option_variables option_count)
+  math(EXPR option_count "${option_count} - 1")
+  foreach(val RANGE ${option_count})
+    list(GET option_variables ${val} option_variable)
+    list(GET option_default_values ${val} option_default_value)
+
+    set(option_str "- ${option_variable}")
+    set(spaces "")
+    string(LENGTH ${option_variable} option_variable_len)
+    math(EXPR space_count "${option_variable_max_len} - ${option_variable_len}")
+    foreach(loop_var RANGE ${space_count})
+      set(option_str "${option_str} ")
+    endforeach()
+
+    set(option_str "${option_str}: ${${option_variable}}")
+
+    if(${option_variable} STREQUAL option_default_value)
+      set(option_str "${option_str} [default]")
+    endif()
+
+    message(STATUS "${option_str}")
+  endforeach()
+
+  message(STATUS "")
+endfunction()
+
 #===============================================================================
 # Appends items to a cached list.
 # Usage:
