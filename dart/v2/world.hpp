e8dbf1ecd2d74526e1e6b400bfae447e6f12418d
Jeongseok Lee <jslee02@gmail.com>
2023-12-19 01:06:15 -0800

    Add v2 component

--- dart/v2/world.hpp
+++ dart/v2/world.hpp
@@ -30,63 +30,12 @@
  *   POSSIBILITY OF SUCH DAMAGE.
  */
 
-#include <dart/common/ecs/entt.hpp>
+#pragma once
 
-#include <gtest/gtest.h>
+#include <vector>
 
-namespace {
+namespace dart {
 
-struct position
-{
-  float x;
-  float y;
-};
+//
 
-struct velocity
-{
-  float dx;
-  float dy;
-};
-
-void update(entt::registry& registry)
-{
-  auto view = registry.view<const position, velocity>();
-
-  // use a callback
-  view.each([](const auto& /* pos */, auto& /* vel */) { /* ... */ });
-
-  // use an extended callback
-  view.each([](const auto /* entity */, const auto& /* pos */, auto& /* vel */) { /* ... */ });
-
-  // use a range-for
-  for (auto [entity, pos, vel] : view.each()) {
-    (void)entity;
-    (void)pos;
-    (void)vel;
-    // ...
-  }
-
-  // use forward iterators and get only the components of interest
-  for (auto entity : view) {
-    auto& vel = view.get<velocity>(entity);
-    (void)vel;
-    // ...
-  }
-}
-
-} // namespace
-
-GTEST_TEST(EntityTest, StaticProperties)
-{
-  entt::registry registry;
-
-  for (auto i = 0u; i < 10u; ++i) {
-    const auto entity = registry.create();
-    registry.emplace<position>(entity, i * 1.f, i * 1.f);
-    if (i % 2 == 0) {
-      registry.emplace<velocity>(entity, i * .1f, i * .1f);
-    }
-  }
-
-  update(registry);
-}
+} // namespace dart
