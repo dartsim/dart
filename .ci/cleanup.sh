2b0d03a688fe0b39855d54620043fc8b7e4333a6
Jeongseok Lee <jslee02@gmail.com>
2024-03-13 08:23:16 -0700

    Update actions clean up script

--- .ci/cleanup.sh
+++ .ci/cleanup.sh
@@ -7,28 +7,28 @@ echo "Deleting workflow runs for $org/$repo"
 
 workflows_temp=$(mktemp) # Creates a temporary file to store workflow data.
 
-gh api repos/$org/$repo/actions/workflows | jq -r '.workflows[] | [.id, .path] | @tsv' >$workflows_temp # Lookup workflow
-cat "$workflows_temp"
+gh api repos/$org/$repo/actions/workflows | jq -r '.workflows[] | [.id, .path] | @tsv' > $workflows_temp # Lookup workflow
+cat "$workflows_temp" 
 
 workflows_names=$(awk '{print $2}' $workflows_temp | grep -v "main")
 
 if [ -z "$workflows_names" ]; then
 
-  echo "All workflows are either successful or failed. Nothing to remove"
+    echo "All workflows are either successful or failed. Nothing to remove"
 
 else
 
-  echo "Removing all the workflows that are not successful or failed"
+    echo "Removing all the workflows that are not successful or failed"
 
-  for workflow_name in $workflows_names; do
+    for workflow_name in $workflows_names; do
 
-    workflow_filename=$(basename "$workflow_name")
-    echo "Deleting |$workflow_filename|, please wait..."
+        workflow_filename=$(basename "$workflow_name")
+        echo "Deleting |$workflow_filename|, please wait..."
 
-    gh run list --limit 500 --workflow $workflow_filename --json databaseId |
-      jq -r '.[] | .databaseId' |
-      xargs -I{} gh run delete {} # Delete all workflow runs for workflow name
-  done
+        gh run list --limit 500 --workflow $workflow_filename --json databaseId |
+            jq -r '.[] | .databaseId' |
+            xargs -I{} gh run delete {} # Delete all workflow runs for workflow name
+    done
 fi
 
 rm -rf $workflows_temp
