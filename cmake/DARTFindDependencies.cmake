e9fbb9faf72533d88f7d9efdf3ea79842967d9c7
Jeongseok Lee <jslee02@gmail.com>
2024-03-16 20:46:30 -0700

    [cmake] Fix finding python
    (cherry picked from commit 9e329f2d70eee8e051432f0c51b8bf3ceecb357b)
    (cherry picked from commit ef572ace706e6edf174555491074243b7f747b73)

--- cmake/DARTFindDependencies.cmake
+++ cmake/DARTFindDependencies.cmake
@@ -120,6 +120,8 @@ endif()
 # Optional dependencies
 #=======================
 
+find_package(Python3 COMPONENTS Interpreter Development)
+
 option(DART_SKIP_spdlog "If ON, do not use spdlog even if it is found." OFF)
 mark_as_advanced(DART_SKIP_spdlog)
 dart_find_package(spdlog)
