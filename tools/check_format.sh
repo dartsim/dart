23ec8669b26ca63c581916dc808a1243ffa98a1a
Jeongseok Lee <jslee02@gmail.com>
2024-03-16 18:52:14 -0700

    [ci] Treat checking lint failure as CI failure
    (cherry picked from commit d23005123cf7b8dea129858e06a202fbc18cff5a)

--- tools/check_format.sh
+++ tools/check_format.sh
@@ -4,12 +4,12 @@ if [ "$#" -lt 2 ]; then
   exit 0
 fi
 
-num_changes=`$1 -style=file -output-replacements-xml "${@:2}" | grep -c "<replacement "`
+num_changes=$($1 -style=file -output-replacements-xml "${@:2}" | grep -c "<replacement ")
 
 if [ "$num_changes" = "0" ]; then
   echo "Every file seems to comply with our code convention."
   exit 0
 else
   echo "Found" $num_changes "necessary changes."
-  exit 0
+  exit 1
 fi
