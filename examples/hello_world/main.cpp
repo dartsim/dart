b8ce7b7639a019dc2b48795f4791224782eb343a
Jeongseok Lee <jslee02@gmail.com>
2024-03-25 18:22:37 -0700

    [example] Update hello world

--- examples/hello_world/main.cpp
+++ examples/hello_world/main.cpp
@@ -34,26 +34,45 @@
 
 #include <dart/dart.hpp>
 
+#include <osgShadow/ShadowMap>
+
 using namespace dart;
 
 int main()
 {
-  auto shape
-      = std::make_shared<dynamics::BoxShape>(Eigen::Vector3d(0.3, 0.3, 0.3));
-
   // Create a box-shaped rigid body
   auto skeleton = dynamics::Skeleton::create();
   auto jointAndBody
       = skeleton->createJointAndBodyNodePair<dynamics::FreeJoint>();
   auto body = jointAndBody.second;
-  body->createShapeNodeWith<
+  Eigen::Isometry3d tf = Eigen::Isometry3d::Identity();
+  tf.translation() << 0, 0, 1;
+  tf.linear() = math::expMapRot(Eigen::Vector3d::Random());
+  body->getParentJoint()->setTransformFromParentBodyNode(tf);
+  auto shapeNode = body->createShapeNodeWith<
       dynamics::VisualAspect,
       dynamics::CollisionAspect,
-      dynamics::DynamicsAspect>(shape);
+      dynamics::DynamicsAspect>(
+      std::make_shared<dynamics::BoxShape>(Eigen::Vector3d(0.3, 0.3, 0.3)));
+  body->setInertia(dynamics::Inertia(
+      1, Eigen::Vector3d::Zero(), shapeNode->getShape()->computeInertia(1.0)));
+  shapeNode->getVisualAspect()->setColor(dart::Color::Blue());
 
-  // Create a world and add the rigid body
+  // Create ground
+  auto ground = dynamics::Skeleton::create("ground");
+  auto groundBody
+      = ground->createJointAndBodyNodePair<dynamics::WeldJoint>().second;
+  auto groundShapeNode = groundBody->createShapeNodeWith<
+      dynamics::VisualAspect,
+      dynamics::CollisionAspect,
+      dynamics::DynamicsAspect>(
+      std::make_shared<dynamics::BoxShape>(Eigen::Vector3d(10.0, 10.0, 0.1)));
+  groundShapeNode->getVisualAspect()->setColor(dart::Color::LightGray());
+
+  // Create a world and add the rigid body and ground to the world
   auto world = simulation::World::create();
   world->addSkeleton(skeleton);
+  world->addSkeleton(ground);
 
   // Wrap a WorldNode around it
   ::osg::ref_ptr<gui::osg::RealTimeWorldNode> node
@@ -62,6 +81,15 @@ int main()
   // Create a Viewer and set it up with the WorldNode
   auto viewer = gui::osg::Viewer();
   viewer.addWorldNode(node);
+
+  // Enable shadow
+  auto shadow
+      = dart::gui::osg::WorldNode::createDefaultShadowTechnique(&viewer);
+  if (auto sm = dynamic_cast<::osgShadow::ShadowMap*>(shadow.get())) {
+    auto mapResolution = static_cast<short>(std::pow(2, 12));
+    sm->setTextureSize(::osg::Vec2s(mapResolution, mapResolution));
+  }
+  node->setShadowTechnique(shadow);
 
   viewer.addInstructionText("Press space to start free falling the box.\n");
   std::cout << viewer.getInstructions() << std::endl;
@@ -72,7 +100,7 @@ int main()
   // Adjust the viewpoint of the Viewer
   viewer.getCameraManipulator()->setHomePosition(
       ::osg::Vec3(2.57f, 3.14f, 1.64f),
-      ::osg::Vec3(0.00f, 0.00f, 0.00f),
+      ::osg::Vec3(0.00f, 0.00f, 0.50f),
       ::osg::Vec3(-0.24f, -0.25f, 0.94f));
 
   // We need to re-dirty the CameraManipulator by passing it into the viewer
