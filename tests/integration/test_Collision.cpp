40bb322995ce91fac47b5184ea3372223bb3d479
Jeongseok Lee <jslee02@gmail.com>
2024-03-28 10:56:15 -0700

    w

--- tests/integration/test_Collision.cpp
+++ tests/integration/test_Collision.cpp
@@ -772,7 +772,7 @@ TEST_F(Collision, testCylinderCylinder)
   // fcl_mesh_fcl->setContactPointComputationMethod(FCLCollisionDetector::FCL);
   // testCylinderCylinder(fcl_mesh_fcl);
 
-#if HAVE_ODE
+#if HAVE_ODE && !defined(ODE_WITH_LIBCCD_BOX_CYL)
   auto ode = OdeCollisionDetector::create();
   testCylinderCylinder(ode);
 #endif
