cc9ad5ac43dbfb28f04b61a7ea384abcc3ef0035
Silvio Traversaro <silvio@traversaro.it>
2025-11-04 16:12:54 +0100

    Add version check for Eigen3 in CMake file

--- cmake/DARTFindEigen3.cmake
+++ cmake/DARTFindEigen3.cmake
@@ -7,3 +7,7 @@
 # This file is provided under the "BSD-style" License
 
 find_package(Eigen3 REQUIRED CONFIG)
+
+if (Eigen3_VERSION VERSION_LESS 3.4)
+  message(FATAL_ERROR "Eigen version>=3.4 is required, but found ${Eigen3_VERSION}")
+endif()
