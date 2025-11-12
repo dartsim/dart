22a006bcb90a126ac8d7eea8815d135aab55409c
Jeongseok (JS) Lee <jslee02@users.noreply.github.com>
2025-11-11 16:46:23 -0800

    Rename dart7/dartpy7 prerelease to dart8/dartpy8 (#2148)

---
+++ python/dartpy8/rigid_body_bind.cpp
@@ -0,0 +1,57 @@
+/*
+ * Copyright (c) 2011-2025, The DART development contributors
+ * All rights reserved.
+ *
+ * The list of contributors can be found at:
+ *   https://github.com/dartsim/dart/blob/main/LICENSE
+ *
+ * This file is provided under the following "BSD-style" License:
+ *   Redistribution and use in source and binary forms, with or
+ *   without modification, are permitted provided that the following
+ *   conditions are met:
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above
+ *     copyright notice, this list of conditions and the following
+ *     disclaimer in the documentation and/or other materials provided
+ *     with the distribution.
+ *   THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND
+ *   CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
+ *   INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ *   MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ *   DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
+ *   CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ *   SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ *   LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
+ *   USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+ *   AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ *   LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ *   ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ *   POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include "rigid_body_bind.hpp"
+
+#include <dart8/body/rigid_body.hpp>
+
+#include <nanobind/nanobind.h>
+#include <nanobind/stl/optional.h>
+#include <nanobind/stl/string.h>
+
+namespace nb = nanobind;
+
+namespace dartpy8 {
+
+void defRigidBody(nb::module_& m)
+{
+  // RigidBodyOptions (placeholder - no members yet)
+  nb::class_<dart8::RigidBodyOptions>(m, "RigidBodyOptions").def(nb::init<>());
+  // TODO: Add bindings for RigidBodyOptions fields when they are implemented
+
+  // RigidBody
+  nb::class_<dart8::RigidBody>(m, "RigidBody")
+      .def("get_name", &dart8::RigidBody::getName, "Get the name")
+      .def("is_valid", &dart8::RigidBody::isValid, "Check if valid");
+}
+
+} // namespace dartpy8
