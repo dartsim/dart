194e8a04f0d860e281c2259017d207be9811a3b7
Jeongseok Lee <jslee02@gmail.com>
2025-11-13 11:00:04 -0800

    Add remaining common nanobind bindings

--- python/dartpy_nb/common/module.cpp
+++ python/dartpy_nb/common/module.cpp
@@ -1,8 +1,13 @@
 #include "common/module.hpp"
 
+#include "common/composite.hpp"
 #include "common/logging.hpp"
+#include "common/observer.hpp"
+#include "common/resource.hpp"
+#include "common/resource_retriever.hpp"
 #include "common/stopwatch.hpp"
 #include "common/string.hpp"
+#include "common/subject.hpp"
 #include "common/uri.hpp"
 
 namespace dart::python_nb {
@@ -10,6 +15,11 @@ namespace dart::python_nb {
 void defCommonModule(nanobind::module_& m)
 {
   defLogging(m);
+  defObserver(m);
+  defSubject(m);
+  defComposite(m);
+  defResource(m);
+  defResourceRetriever(m);
   defStopwatch(m);
   defString(m);
   defUri(m);
