aa7ab3f6ca121fe9f4551adfcf9a436f32a051e5
Jeongseok Lee <jslee02@gmail.com>
2025-11-20 08:38:31 -0800

    Silence deprecated ResourceRetriever binding in dartpy_nb

--- python/dartpy_nb/common/resource_retriever.cpp
+++ python/dartpy_nb/common/resource_retriever.cpp
@@ -1,5 +1,6 @@
 #include "common/resource_retriever.hpp"
 
+#include "dart/common/Diagnostics.hpp"
 #include "dart/common/LocalResourceRetriever.hpp"
 #include "dart/common/ResourceRetriever.hpp"
 
@@ -15,11 +16,13 @@ void defResourceRetriever(nb::module_& m)
 {
   using ResourceRetriever = dart::common::ResourceRetriever;
 
+  DART_SUPPRESS_DEPRECATED_BEGIN
   nb::class_<ResourceRetriever>(m, "ResourceRetriever")
       .def("exists", &ResourceRetriever::exists, nb::arg("uri"))
       .def("retrieve", &ResourceRetriever::retrieve, nb::arg("uri"))
       .def("readAll", &ResourceRetriever::readAll, nb::arg("uri"))
       .def("getFilePath", &ResourceRetriever::getFilePath, nb::arg("uri"));
+  DART_SUPPRESS_DEPRECATED_END
 
   using LocalResourceRetriever = dart::common::LocalResourceRetriever;
   nb::class_<LocalResourceRetriever, ResourceRetriever>(
