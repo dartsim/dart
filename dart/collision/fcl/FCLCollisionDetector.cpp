ecb28fe6ae4b464bed3564857ce4c55ea14bff4c
Jeongseok Lee <jslee02@gmail.com>
2025-11-23 09:38:42 -0800

    Add FCL mesh contact regression guard and test

--- dart/collision/fcl/FCLCollisionDetector.cpp
+++ dart/collision/fcl/FCLCollisionDetector.cpp
@@ -1103,9 +1103,21 @@ bool collisionCallback(
   ::fcl::collide(o1, o2, fclRequest, fclResult);
 
   if (result) {
+    const bool usingMeshContacts
+        = (collData->primitiveShapeType == FCLCollisionDetector::MESH);
+    const bool forcingMeshFallback = usingMeshContacts
+                                     && collData->contactPointComputationMethod
+                                            == FCLCollisionDetector::FCL;
+
     // Post processing -- converting fcl contact information to ours if needed
-    if (FCLCollisionDetector::DART == collData->contactPointComputationMethod
-        && FCLCollisionDetector::MESH == collData->primitiveShapeType) {
+    if (usingMeshContacts) {
+      if (forcingMeshFallback) {
+        DART_WARN_ONCE(
+            "FCL mesh contact-point computation is known to be inaccurate "
+            "(see flexible-collision-library/fcl#106); falling back to DART's "
+            "mesh contact handler.");
+      }
+
       postProcessDART(fclResult, o1, o2, option, *result);
     } else {
       postProcessFCL(fclResult, o1, o2, option, *result);
