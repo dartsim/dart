473267330acc66f40b1470995efc636080e2ec94
Jeongseok Lee <jslee02@gmail.com>
2025-11-23 07:16:44 -0800

    Restore lcpsolver compatibility shim

--- dart/lcpsolver/detail/DeprecatedNamespace.hpp
+++ dart/lcpsolver/detail/DeprecatedNamespace.hpp
@@ -13,4 +13,16 @@
   #endif
 #endif
 
+namespace dart {
+namespace math {
+namespace lcp {
+using namespace ::dart::math;
+} // namespace lcp
+} // namespace math
+namespace [[deprecated("dart::lcpsolver is deprecated; include dart/math/lcp "
+                       "headers and use dart::math instead")]] lcpsolver {
+using namespace math::lcp;
+} // namespace lcpsolver
+} // namespace dart
+
 #endif // DART_LCPSOLVER_DETAIL_DEPRECATEDNAMESPACE_HPP_
