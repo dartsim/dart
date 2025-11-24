9196fcef789139c9b42d0499d4b70c6fcb768854
Jeongseok Lee <jslee02@gmail.com>
2025-11-23 18:05:19 -0800

    lcpsolver: align deprecated namespace with inline namespace

--- dart/lcpsolver/detail/DeprecatedNamespace.hpp
+++ dart/lcpsolver/detail/DeprecatedNamespace.hpp
@@ -2,6 +2,7 @@
 #define DART_LCPSOLVER_DETAIL_DEPRECATEDNAMESPACE_HPP_
 
 #include "dart/common/Deprecated.hpp"
+#include <dart/common/Namespace.hpp>
 
 #if !defined(DART_LCPSOLVER_SUPPRESS_DEPRECATED_HEADER_WARNING)
   #if defined(_MSC_VER)
@@ -13,19 +14,17 @@
   #endif
 #endif
 
-namespace dart {
+DART_INLINE_NAMESPACE_BEGIN
 namespace math {
 namespace lcp {
 } // namespace lcp
 } // namespace math
-} // namespace dart
 
-namespace dart {
 namespace
     [[deprecated("dart::lcpsolver is deprecated; include dart/math/lcp headers "
                  "and use dart::math instead")]] lcpsolver {
 using namespace math::lcp;
 } // namespace lcpsolver
-} // namespace dart
+DART_INLINE_NAMESPACE_END
 
 #endif // DART_LCPSOLVER_DETAIL_DEPRECATEDNAMESPACE_HPP_
