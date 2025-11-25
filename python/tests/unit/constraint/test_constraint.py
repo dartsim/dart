176fb3486c91270a0efb1d57a4447110b68b50f8
Jeongseok Lee <jslee02@gmail.com>
2025-11-24 16:38:28 -0800

    Port constraint unit tests to flattened dartpy API

--- python/tests/unit/constraint/test_constraint.py
+++ python/tests/unit/constraint/test_constraint.py
@@ -39,45 +39,45 @@ def test_ball_joint_constraint():
 
 
 def test_revolute_joint_constraint():
-    world = dart.utils.SkelParser.readWorld("dart://sample/skel/chain.skel")
-    world.setGravity([0, -9.81, 0])
-    world.setTimeStep(1.0 / 2000)
+    world = dart.io.SkelParser.read_world("dart://sample/skel/chain.skel")
+    world.set_gravity([0, -9.81, 0])
+    world.set_time_step(1.0 / 2000)
 
-    chain = world.getSkeleton(0)
-    init_pose = np.zeros(chain.getNumDofs())
+    chain = world.get_skeleton(0)
+    init_pose = np.zeros(chain.get_num_dofs())
     init_pose[[20, 23, 26, 29]] = np.pi * 0.4
-    chain.setPositions(init_pose)
+    chain.set_positions(init_pose)
 
-    for i in range(chain.getNumJoints()):
-        joint = chain.getJoint(i)
-        for j in range(joint.getNumDofs()):
-            joint.setDampingCoefficient(j, 0.01)
+    for i in range(chain.get_num_joints()):
+        joint = chain.get_joint(i)
+        for j in range(joint.get_num_dofs()):
+            joint.set_damping_coefficient(j, 0.01)
 
-    bd1 = chain.getBodyNode("link 6")
-    bd2 = chain.getBodyNode("link 10")
+    bd1 = chain.get_body_node("link 6")
+    bd2 = chain.get_body_node("link 10")
     offset = [0, 0.025, 0]
-    joint_pos = bd1.getTransform().multiply(offset)
+    joint_pos = bd1.get_transform().multiply(offset)
     axis = [0, 1, 0]
 
     constraint = dart.constraint.RevoluteJointConstraint(
         bd1, bd2, joint_pos, axis, axis
     )
     assert (
-        constraint.getType()
-        == dart.constraint.RevoluteJointConstraint.getStaticType()
+        constraint.get_type()
+        == dart.constraint.RevoluteJointConstraint.get_static_type()
     )
 
-    constraint_solver = world.getConstraintSolver()
-    constraint_solver.addConstraint(constraint)
+    constraint_solver = world.get_constraint_solver()
+    constraint_solver.add_constraint(constraint)
 
     for _ in range(200):
         world.step()
-        pos1 = bd1.getTransform().multiply(offset)
-        pos2 = bd2.getTransform().multiply(offset)
+        pos1 = bd1.get_transform().multiply(offset)
+        pos2 = bd2.get_transform().multiply(offset)
         assert np.linalg.norm(pos1 - pos2) < 5e-2
 
-    axis1 = bd1.getTransform().rotation().dot(np.array([0.0, 1.0, 0.0]))
-    axis2 = bd2.getTransform().rotation().dot(np.array([0.0, 1.0, 0.0]))
+    axis1 = bd1.get_transform().rotation().dot(np.array([0.0, 1.0, 0.0]))
+    axis2 = bd2.get_transform().rotation().dot(np.array([0.0, 1.0, 0.0]))
     axis1 /= np.linalg.norm(axis1)
     axis2 /= np.linalg.norm(axis2)
     assert np.dot(axis1, axis2) > 0.2
