373d4403a8c0d880f59994390505b94725b7e99b
Jeongseok Lee <jslee02@gmail.com>
2025-12-01 11:23:08 -0800

    Enable VSG builds in pixi tasks and add VSG example

---
+++ examples/vsg_hello_world/main.cpp
@@ -0,0 +1,62 @@
+/*
+ * Minimal VulkanSceneGraph viewer example for DART.
+ */
+
+#include <dart/gui/vsg/Viewer.hpp>
+
+#include <dart/dynamics/BoxShape.hpp>
+#include <dart/dynamics/ShapeNode.hpp>
+#include <dart/dynamics/Skeleton.hpp>
+#include <dart/dynamics/WeldJoint.hpp>
+#include <dart/simulation/World.hpp>
+
+#include <Eigen/Geometry>
+
+using namespace dart;
+
+namespace {
+
+dynamics::SkeletonPtr createFloor()
+{
+  auto floor = dynamics::Skeleton::create("floor");
+  auto [joint, body] = floor->createJointAndBodyNodePair<dynamics::WeldJoint>();
+  auto shape = std::make_shared<dynamics::BoxShape>(
+      Eigen::Vector3d(5.0, 5.0, 0.2));
+  auto* shapeNode = body->createShapeNodeWith<
+      dynamics::VisualAspect,
+      dynamics::CollisionAspect,
+      dynamics::DynamicsAspect>(shape);
+  shapeNode->getVisualAspect()->setColor(Eigen::Vector4d(0.8, 0.8, 0.8, 1.0));
+  body->setTranslation(Eigen::Vector3d(0.0, 0.0, -0.1));
+  return floor;
+}
+
+dynamics::SkeletonPtr createBox()
+{
+  auto box = dynamics::Skeleton::create("box");
+  auto [joint, body] = box->createJointAndBodyNodePair<dynamics::FreeJoint>();
+  auto shape
+      = std::make_shared<dynamics::BoxShape>(Eigen::Vector3d::Constant(0.2));
+  auto* shapeNode = body->createShapeNodeWith<
+      dynamics::VisualAspect,
+      dynamics::CollisionAspect,
+      dynamics::DynamicsAspect>(shape);
+  shapeNode->getVisualAspect()->setColor(Eigen::Vector4d(0.2, 0.4, 0.8, 1.0));
+  body->setTranslation(Eigen::Vector3d(0.0, 0.0, 1.0));
+  return box;
+}
+
+} // namespace
+
+int main()
+{
+  auto world = std::make_shared<simulation::World>();
+  world->setName("vsg-example");
+  world->addSkeleton(createFloor());
+  world->addSkeleton(createBox());
+
+  gui::vsg::Viewer viewer(world);
+  viewer.simulate(true);
+  viewer.setNumStepsPerCycle(4);
+  viewer.run();
+}
