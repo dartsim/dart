f16f0e67446701f0de87e4c76bcbece83dcbd4b2
Jeongseok Lee <jslee02@gmail.com>
2026-01-19 09:10:12 -0800

    feat(collision): add standalone shape classes
    Implement collision-only shape representations:
    - SphereShape: radius-based sphere
    - BoxShape: half-extents based box
    
    Shapes are decoupled from dynamics for pure collision testing.
    Each provides computeLocalAabb() for broad-phase integration.
    
    8 tests covering shape construction and AABB computation.
    
    Part of experimental collision module - Task 4.

---
+++ tests/unit/collision/experimental/test_shapes.cpp
@@ -0,0 +1,117 @@
+/*
+ * Copyright (c) 2011, The DART development contributors
+ * All rights reserved.
+ *
+ * The list of contributors can be found at:
+ *   https://github.com/dartsim/dart/blob/main/LICENSE
+ *
+ * This file is provided under the following "BSD-style" License:
+ *   Redistribution and use in source and binary forms, with or
+ *   without modification, are permitted provided that the following
+ *   conditions are met:
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above
+ *     copyright notice, this list of conditions and the following
+ *     disclaimer in the documentation and/or other materials provided
+ *     with the distribution.
+ *   THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND
+ *   CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
+ *   INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ *   MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ *   DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
+ *   CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ *   SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ *   LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
+ *   USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+ *   AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ *   LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ *   ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ *   POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include <dart/collision/experimental/shapes/shape.hpp>
+
+#include <gtest/gtest.h>
+
+using namespace dart::collision::experimental;
+
+TEST(SphereShape, Construction)
+{
+  SphereShape sphere(2.5);
+
+  EXPECT_EQ(sphere.getType(), ShapeType::Sphere);
+  EXPECT_DOUBLE_EQ(sphere.getRadius(), 2.5);
+}
+
+TEST(SphereShape, ComputeLocalAabb)
+{
+  SphereShape sphere(3.0);
+
+  auto aabb = sphere.computeLocalAabb();
+
+  EXPECT_EQ(aabb.min, Eigen::Vector3d(-3, -3, -3));
+  EXPECT_EQ(aabb.max, Eigen::Vector3d(3, 3, 3));
+}
+
+TEST(SphereShape, SmallRadius)
+{
+  SphereShape sphere(0.001);
+
+  EXPECT_DOUBLE_EQ(sphere.getRadius(), 0.001);
+
+  auto aabb = sphere.computeLocalAabb();
+  EXPECT_NEAR(aabb.volume(), 8e-9, 1e-15);
+}
+
+TEST(BoxShape, Construction)
+{
+  BoxShape box(Eigen::Vector3d(1, 2, 3));
+
+  EXPECT_EQ(box.getType(), ShapeType::Box);
+  EXPECT_EQ(box.getHalfExtents(), Eigen::Vector3d(1, 2, 3));
+}
+
+TEST(BoxShape, ComputeLocalAabb)
+{
+  BoxShape box(Eigen::Vector3d(1, 2, 3));
+
+  auto aabb = box.computeLocalAabb();
+
+  EXPECT_EQ(aabb.min, Eigen::Vector3d(-1, -2, -3));
+  EXPECT_EQ(aabb.max, Eigen::Vector3d(1, 2, 3));
+}
+
+TEST(BoxShape, CubeAabb)
+{
+  BoxShape box(Eigen::Vector3d(0.5, 0.5, 0.5));
+
+  auto aabb = box.computeLocalAabb();
+
+  EXPECT_EQ(aabb.center(), Eigen::Vector3d::Zero());
+  EXPECT_EQ(aabb.halfExtents(), Eigen::Vector3d(0.5, 0.5, 0.5));
+  EXPECT_DOUBLE_EQ(aabb.volume(), 1.0);
+}
+
+TEST(Shape, Polymorphism)
+{
+  std::unique_ptr<Shape> sphere = std::make_unique<SphereShape>(1.0);
+  std::unique_ptr<Shape> box = std::make_unique<BoxShape>(Eigen::Vector3d(1, 1, 1));
+
+  EXPECT_EQ(sphere->getType(), ShapeType::Sphere);
+  EXPECT_EQ(box->getType(), ShapeType::Box);
+
+  auto sphereAabb = sphere->computeLocalAabb();
+  auto boxAabb = box->computeLocalAabb();
+
+  EXPECT_DOUBLE_EQ(sphereAabb.volume(), 8.0);
+  EXPECT_DOUBLE_EQ(boxAabb.volume(), 8.0);
+}
+
+TEST(ShapeType, EnumValues)
+{
+  EXPECT_NE(ShapeType::Sphere, ShapeType::Box);
+  EXPECT_NE(ShapeType::Box, ShapeType::Capsule);
+  EXPECT_NE(ShapeType::Capsule, ShapeType::Cylinder);
+  EXPECT_NE(ShapeType::Cylinder, ShapeType::Mesh);
+}
