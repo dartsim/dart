cab80d218dc70446ffbc64265375c01d7313ab80
Jeongseok Lee <jslee02@gmail.com>
2026-06-19 12:05:53 -0700

    Preserve Dantzig legacy ABI wrappers

--- tests/unit/lcpsolver/test_DantzigCompat.cpp
+++ tests/unit/lcpsolver/test_DantzigCompat.cpp
@@ -1,7 +1,34 @@
 #include "dart/external/odelcpsolver/lcp.h"
+#include "dart/external/odelcpsolver/matrix.h"
 #include "dart/lcpsolver/dantzig/lcp.h"
 
 #include <gtest/gtest.h>
+
+#undef dSetZero
+
+namespace dart {
+namespace external {
+namespace ode {
+
+using dReal = ::dart::lcpsolver::dantzig::dReal;
+
+bool dSolveLCP(
+    int n,
+    dReal* A,
+    dReal* x,
+    dReal* b,
+    dReal* w,
+    int nub,
+    dReal* lo,
+    dReal* hi,
+    int* findex,
+    bool earlyTermination = false);
+
+void dSetZero(dReal* a, int n);
+
+} // namespace ode
+} // namespace external
+} // namespace dart
 
 namespace {
 
@@ -31,4 +58,14 @@ TEST(DantzigCompat, SolvesThroughDartOwnedNamespace)
 TEST(DantzigCompat, SolvesThroughLegacyExternalNamespace)
 {
   expectUnboundedSolve(::dart::external::ode::dSolveLCP);
+}
+
+TEST(DantzigCompat, LinksLegacyMatrixSymbol)
+{
+  double values[2] = {1.0, -2.0};
+
+  ::dart::external::ode::dSetZero(values, 2);
+
+  EXPECT_DOUBLE_EQ(0.0, values[0]);
+  EXPECT_DOUBLE_EQ(0.0, values[1]);
 }
--- tools/freebsd/patches/patch-dart_lcpsolver_dantzig_matrix.cpp
+++ tools/freebsd/patches/patch-dart_lcpsolver_dantzig_matrix.cpp
@@ -1,5 +1,5 @@
---- dart/external/odelcpsolver/matrix.cpp.orig	2018-11-20 16:43:32 UTC
-+++ dart/external/odelcpsolver/matrix.cpp
+--- dart/lcpsolver/dantzig/matrix.cpp.orig	2018-11-20 16:43:32 UTC
++++ dart/lcpsolver/dantzig/matrix.cpp
 @@ -23,6 +23,8 @@
  //#include "config.h"
  #ifdef __APPLE__
