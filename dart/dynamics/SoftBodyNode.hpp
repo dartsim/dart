8abd90fe49f3b3d86114240ed933719a28b21876
Jeongseok Lee <jslee02@gmail.com>
2026-07-07 21:28:11 -0700

    Keep soft mesh shape cached refresh

--- dart/dynamics/SoftBodyNode.hpp
+++ dart/dynamics/SoftBodyNode.hpp
@@ -60,7 +60,6 @@ public:
   friend class Skeleton;
   friend class collision::DARTCollisionObject;
   friend class collision::FCLCollisionObject;
-  friend class SoftMeshShape;
   friend class PointMass;
   friend class PointMassNotifier;
 
